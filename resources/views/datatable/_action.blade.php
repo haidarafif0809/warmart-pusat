295eb5ede70413db3ba296008b4ad3c2528bd219
Riko Pernando <rikopernando96@gmail.com>
2017-10-12 13:23:36 +0700

    Crud Master Barang
    Crud Master Barang, Browser Testnya masih dikerjakan

--- resources/views/datatable/_action.blade.php
+++ resources/views/datatable/_action.blade.php
@@ -14,10 +14,13 @@
 	@endif
 
 
+    @if(Auth::user()->tipe_user == 4)
 
 	{!! Form::model($model, ['url' => $form_url, 'method' => 'delete', 'class' => 'form-inline js-confirm', 'data-confirm' => $confirm_message]) !!}
 
 		<a href="{{ $edit_url }}" class="btn btn-sm btn-success">Ubah</a> |
 
 		{!! Form::submit('Hapus',['class'=>'btn btn-sm btn-danger js-confirm']) !!}
-{!! Form::close() !!}
+	{!! Form::close() !!}
+
+    @endif
