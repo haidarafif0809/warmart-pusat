1f64c1a22472a84044b1aff6570f2c5112f68a8f
Unknown <ghozam.alghifari96@gmail.com>
2017-10-13 14:29:55 +0700

    Memperbarui HTTP TEST Register

--- tests/Unit/RegisterCustomerTest.php
+++ tests/Unit/RegisterCustomerTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions; 
 use URL;
+use App\User;
 
 class RegisterCustomerTest extends TestCase
 {
@@ -30,8 +31,7 @@ class RegisterCustomerTest extends TestCase
 
 
     public function testHTTPRegisterCustomer(){
-             
-
+              
         $response = $this->json('POST', url('/register'),[
             'name' => 'UserCustomerTestHttp',
             'email' => 'usercustomertesthttp@gmail.com',
@@ -55,8 +55,7 @@ class RegisterCustomerTest extends TestCase
 	            'no_telp' => '082282107750',
 	            'alamat' => 'Test Alamat Http',   
                 'tipe_user' => '3'
-            ]); 
-
+            ]);  
     }
  
     public function testHTTPVerifikasiCustomerSalahKodeVerifikasi(){
@@ -87,18 +86,56 @@ class RegisterCustomerTest extends TestCase
                 'tipe_user' => '3'
             ]);   
 
-        $response = $this->json('POST', route('user.proses_kirim_kode_verifikasi', '082282107750'),[
+        $response = $this->json('PUT', route('user.proses_kirim_kode_verifikasi', '082282107750'),[
             'no_telp' => '082282107750',
             'kode_verifikasi' => '9820',
 
         ]);
 
-
         $response->assertStatus(302)
-                 ->assertRedirect(route('user.proses_kirim_kode_verifikasi', '082282107750'));
+                 ->assertRedirect(url('kirim-kode-verifikasi?nomor=082282107750&status=0'));
 
 
         $response2 = $this->get($response->headers->get('location'))->assertSee('Mohon Maaf Kode Verfikasi Yang Anda Isi Tidak Sama');
+    }
+
+    public function testHTTPVerifikasiCustomer(){
+             
+
+        $response = $this->json('POST', url('/register'),[
+            'name' => 'UserCustomerTestHttp',
+            'email' => 'usercustomertesthttp@gmail.com',
+            'no_telp' => '082282107750',
+            'alamat' => 'Test Alamat Http', 
+            'password' => 'rahasia', 
+            'password_confirmation' => 'rahasia', 
+            'id_register' => '1', 
+
+        ]);
+
+        $response->assertStatus(302)
+                 ->assertRedirect(url('kirim-kode-verifikasi?nomor=082282107750&status=0'));
+
+
+        $response2 = $this->get($response->headers->get('location'))->assertSee('Silakan input nomor verifikasi yang terkirim melalui SMS ke no 082282107750');
+
+           $this->assertDatabaseHas('users',[
+                'name' => 'UserCustomerTestHttp',
+                'email' => 'usercustomertesthttp@gmail.com',
+                'no_telp' => '082282107750',
+                'alamat' => 'Test Alamat Http',   
+                'tipe_user' => '3'
+            ]);   
+        $user = User::where('no_telp','082282107750')->first();
+        $response = $this->json('PUT', route('user.proses_kirim_kode_verifikasi', '082282107750'),[
+            'no_telp' => '082282107750',
+            'kode_verifikasi' => $user->kode_verifikasi,
+
+        ]);
+
+        $response->assertStatus(302)
+                 ->assertRedirect(url('home'));
+ 
     }
 
 } 
--- tests/Unit/RegisterKomunitasTest.php
+++ tests/Unit/RegisterKomunitasTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions; 
 use URL;
+use App\User;
 
 class RegisterKomunitasTest extends TestCase
 {
@@ -58,4 +59,83 @@ class RegisterKomunitasTest extends TestCase
 
     }
  
+    public function testHTTPVerifikasiKomunitasSalahKodeVerifikasi(){
+             
+
+        $response = $this->json('POST', url('/register'),[
+            'name' => 'UserKomunitasTestHttp',
+            'email' => 'userkomunitastesthttp@gmail.com',
+            'no_telp' => '082282107750',
+            'alamat' => 'Test Alamat Http', 
+            'password' => 'rahasia', 
+            'password_confirmation' => 'rahasia', 
+            'id_register' => '2', 
+
+        ]);
+
+        $response->assertStatus(302)
+                 ->assertRedirect(url('kirim-kode-verifikasi?nomor=082282107750&status=0'));
+
+
+        $response2 = $this->get($response->headers->get('location'))->assertSee('Silakan input nomor verifikasi yang terkirim melalui SMS ke no 082282107750');
+
+           $this->assertDatabaseHas('users',[
+                'name' => 'UserKomunitasTestHttp',
+                'email' => 'userkomunitastesthttp@gmail.com',
+                'no_telp' => '082282107750',
+                'alamat' => 'Test Alamat Http',   
+                'tipe_user' => '2'
+            ]);   
+
+        $response = $this->json('PUT', route('user.proses_kirim_kode_verifikasi', '082282107750'),[
+            'no_telp' => '082282107750',
+            'kode_verifikasi' => '9820',
+
+        ]);
+
+        $response->assertStatus(302)
+                 ->assertRedirect(url('kirim-kode-verifikasi?nomor=082282107750&status=0'));
+
+
+        $response2 = $this->get($response->headers->get('location'))->assertSee('Mohon Maaf Kode Verfikasi Yang Anda Isi Tidak Sama');
+    }
+
+    public function testHTTPVerifikasiKomunitas(){
+             
+
+        $response = $this->json('POST', url('/register'),[
+            'name' => 'UserKomunitasTestHttp',
+            'email' => 'userkomunitastesthttp@gmail.com',
+            'no_telp' => '082282107750',
+            'alamat' => 'Test Alamat Http', 
+            'password' => 'rahasia', 
+            'password_confirmation' => 'rahasia', 
+            'id_register' => '2', 
+
+        ]);
+
+        $response->assertStatus(302)
+                 ->assertRedirect(url('kirim-kode-verifikasi?nomor=082282107750&status=0'));
+
+
+        $response2 = $this->get($response->headers->get('location'))->assertSee('Silakan input nomor verifikasi yang terkirim melalui SMS ke no 082282107750');
+
+           $this->assertDatabaseHas('users',[
+                'name' => 'UserKomunitasTestHttp',
+                'email' => 'userkomunitastesthttp@gmail.com',
+                'no_telp' => '082282107750',
+                'alamat' => 'Test Alamat Http',   
+                'tipe_user' => '2'
+            ]);   
+        $user = User::where('no_telp','082282107750')->first();
+        $response = $this->json('PUT', route('user.proses_kirim_kode_verifikasi', '082282107750'),[
+            'no_telp' => '082282107750',
+            'kode_verifikasi' => $user->kode_verifikasi,
+
+        ]);
+
+        $response->assertStatus(302)
+                 ->assertRedirect(url('home'));
+ 
+    }
 }
