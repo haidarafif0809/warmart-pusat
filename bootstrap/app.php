454306d5d3ce9c4dbd295aa957d3a2ee59a4a41a
fahrizalrahman <fahrizal95rhm@gmail.com>
2017-10-18 11:45:10 +0700

    Perbaikan landing page warmart utama revisi

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,10 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+$app->register(
+	Artesaos\SEOTools\Providers\SEOToolsServiceProvider::class
+);
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
