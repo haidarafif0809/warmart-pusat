9b605d0cd3d89f70ef66c5f01b83d22bc2de4593
fahrizalrahman <fahrizal95rhm@gmail.com>
2017-10-19 11:13:26 +0700

    tambah title di head ,tanpa seometa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,8 @@
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
 
+        <title>War-Mart.id</title>
+
       <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
--- resources/views/layouts/app_login.blade.php
+++ resources/views/layouts/app_login.blade.php
@@ -7,6 +7,10 @@
     <link rel="icon" type="image/png" href="img/favicon.png" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+       
+        <title>War-Mart.id</title>
+
+
     <meta name="viewport" content="width=device-width" />
     <!-- Bootstrap core CSS     -->
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" />
