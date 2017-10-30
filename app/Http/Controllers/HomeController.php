08837080a593d6044988d0d34342f6cd0b9b6e55
Unknown <ghozam.alghifari96@gmail.com>
2017-10-30 11:00:36 +0700

    Membuat Dasboar Warung

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,6 +12,10 @@ use Twitter;
 use App\User;
 use App\Barang;
 use App\Error;
+use Auth;
+use App\TransaksiKas;
+use DB;
+use App\Hpp;
 
 
 class HomeController extends Controller
@@ -23,7 +27,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-      
+
     }
 
     /**
@@ -48,6 +52,7 @@ class HomeController extends Controller
 
     public function dashboard()
     {
+        //DASBOARD ADMIN
         $jumlah_komunitas = User::where('tipe_user','2')->count();
         $jumlah_customer = User::where('tipe_user','3')->count();
         $jumlah_warung = User::where('tipe_user','4')->count();
@@ -55,7 +60,22 @@ class HomeController extends Controller
         $jumlah_komunitas_tervalidasi = User::where('tipe_user','2')->where('konfirmasi_admin','1')->count();
         $produk = Barang::count();
         $error_log = Error::count();
-        return view('home',['jumlah_komunitas'=>$jumlah_komunitas,'jumlah_customer'=>$jumlah_customer,'jumlah_warung'=>$jumlah_warung,'jumlah_warung_tervalidasi'=>$jumlah_warung_tervalidasi,'jumlah_komunitas_tervalidasi'=>$jumlah_komunitas_tervalidasi,'produk'=>$produk,'error_log'=>$error_log]);
+
+        //DASBOARD WARUNG
+        $user_warung = Auth::user()->id_warung;
+        $produk_warung = Barang::where('id_warung',$user_warung)->count();
+        $transaksi_kas = TransaksiKas::select([DB::raw('IFNULL(SUM(jumlah_masuk),0) - IFNULL(SUM(jumlah_keluar),0) as jumlah_kas')])->where('warung_id',$user_warung)->first();
+        $jumlah_kas_masuk = TransaksiKas::select([DB::raw('IFNULL(SUM(jumlah_masuk),0) as total_kas_masuk')])->where('warung_id',$user_warung)->first();
+        $jumlah_kas_keluar = TransaksiKas::select([DB::raw('IFNULL(SUM(jumlah_keluar),0) as total_kas_keluar')])->where('warung_id',$user_warung)->first();
+
+        $stok_masuk = Hpp::select([DB::raw('IFNULL(SUM(jumlah_masuk),0) as jumlah_item_masuk')])->where('warung_id', $user_warung)->first(); 
+        $stok_keluar = Hpp::select([DB::raw('IFNULL(SUM(jumlah_keluar),0) as jumlah_item_keluar')])->where('warung_id', $user_warung)->first(); 
+        $nila_masuk = Hpp::select([DB::raw('IFNULL(SUM(total_nilai),0) as total_masuk')])->where('jenis_hpp',1)->where('warung_id',$user_warung)->first();
+        $nila_keluar = Hpp::select([DB::raw('IFNULL(SUM(total_nilai),0) as total_keluar')])->where('jenis_hpp',2)->where('warung_id',$user_warung)->first();
+        $prose_total_persedian = $nila_masuk->total_masuk - $nila_keluar->total_keluar;
+        $total_persedian = number_format($prose_total_persedian,0,',','.');
+        
+        return view('home',['jumlah_komunitas'=>$jumlah_komunitas,'jumlah_customer'=>$jumlah_customer,'jumlah_warung'=>$jumlah_warung,'jumlah_warung_tervalidasi'=>$jumlah_warung_tervalidasi,'jumlah_komunitas_tervalidasi'=>$jumlah_komunitas_tervalidasi,'produk'=>$produk,'error_log'=>$error_log,'produk_warung'=>$produk_warung,'transaksi_kas'=>$transaksi_kas,'jumlah_kas_masuk'=>$jumlah_kas_masuk,'jumlah_kas_keluar'=>$jumlah_kas_keluar,'stok_masuk'=>$stok_masuk,'stok_keluar'=>$stok_keluar,'total_persedian'=>$total_persedian]);
     }
 
     public function sms(){
