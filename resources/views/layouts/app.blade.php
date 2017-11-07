1a958ad90e4e66cbdef01aaa55e974b0d594f985
Unknown <ghozam.alghifari96@gmail.com>
2017-11-07 15:58:57 +0700

    Memperbaiki Favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
-	<link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img/favicon.jpg') }}" />
-	<link rel="icon" type="image/png" href="{{ asset('img/favicon.jpg') }}" />
+	<link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img/favicon.png') }}" />
+	<link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 	<meta name="viewport" content="width=device-width" />
