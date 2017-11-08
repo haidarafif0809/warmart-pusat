45e5d4aed1c9dbdd44c8aa3ba12a6d96ddb2d649
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2017-11-08 13:52:23 +0700

    Perbaikan browser testing bank test

--- tests/Browser/BankTest.php
+++ tests/Browser/BankTest.php
@@ -17,20 +17,20 @@ class BankTest extends DuskTestCase
      * @return void
      */
     public function testTambahBank() {
-        $this->browse(function ($masterBank, $second) {
-           $masterBank->loginAs(User::find(1))
-                      ->visit('/bank')
-                      ->clickLink('Tambah Bank')
-                      ->type('nama_bank','MANDIRI')
-                      ->type('atas_nama','Afif Maulana')
-                      ->type('no_rek','01204656596');
-            $masterBank->script("document.getElementById('tampil_customer').checked = true;"); 
-          $masterBank->press('#btnSimpanBank')
-                     ->whenAvailable('.swal-modal', function ($modal) {
-                            $modal->assertSee('Sukses : Berhasil Menambah Bank MANDIRI');
-               
-                        });
-        });
+      $this->browse(function ($masterBank, $second) {
+       $masterBank->loginAs(User::find(1))
+       ->visit('/dashboard#/bank')
+       ->clickLink('Tambah Bank')
+       ->type('nama_bank','MANDIRI')
+       ->type('atas_nama','Afif Maulana')
+       ->type('no_rek','01204656596');
+       $masterBank->script("document.getElementById('tampil_customer').checked = true;"); 
+       $masterBank->press('#btnSimpanBank')
+       ->whenAvailable('.swal-modal', function ($modal) {
+        $modal->assertSee('Sukses : Berhasil Menambah Bank MANDIRI');
+
+      });
+     });
     }
 
     public function testUbahBank() {
@@ -39,23 +39,23 @@ class BankTest extends DuskTestCase
 
       $this->browse(function ($masterBank, $second)use($bank) {
         $masterBank->loginAs(User::find(1))
-                  ->visit('/bank')
-                  ->assertSeeLink('Tambah Bank')
-                  ->whenAvailable('.data-ada', function ($modal) use ($bank) {
-                            $modal->click('#edit-'.$bank->id);
-               
-                  })
-                  ->assertSee('Edit Bank')
-                  ->type('nama_bank','MANDIRI SYARIAH')
-                  ->type('atas_nama','Rindang Ramadhan')
-                  ->type('no_rek','01245286425');
-              $masterBank->script("document.getElementById('tampil_customer').checked = true;"); 
-                  $masterBank->press('#btnSimpanBank')
-                  ->whenAvailable('.swal-modal', function ($modal) {
-                            $modal->assertSee('Berhasil Mengubah Bank!');
-               
-                        });
+        ->visit('/dashboard#/bank')
+        ->assertSeeLink('Tambah Bank')
+        ->whenAvailable('.data-ada', function ($modal) use ($bank) {
+          $modal->click('#edit-'.$bank->id);
+
+        })
+        ->assertSee('Edit Bank')
+        ->type('nama_bank','MANDIRI SYARIAH')
+        ->type('atas_nama','Rindang Ramadhan')
+        ->type('no_rek','01245286425');
+        $masterBank->script("document.getElementById('tampil_customer').checked = true;"); 
+        $masterBank->press('#btnSimpanBank')
+        ->whenAvailable('.swal-modal', function ($modal) {
+          $modal->assertSee('Berhasil Mengubah Bank!');
+
         });
+      });
 
     } 
 
@@ -65,36 +65,35 @@ class BankTest extends DuskTestCase
 
       $this->browse(function ($masterBank, $second)use($bank) {
         $masterBank->loginAs(User::find(1))
-                  ->visit('/bank')
-                  ->assertSeeLink('Tambah Bank')
-                  ->whenAvailable('.data-ada', function ($modal) use ($bank) {
-                            $modal->click('#delete-'.$bank->id) 
-                            ->assertDialogOpened('Anda Yakin Ingin Menghapus Bank MANDIRI SYARIAH ?');
-               
-                  })
-                  ->driver->switchTo()->alert()->accept();
-                  $masterBank->whenAvailable('.swal-modal', function ($modal) {
-                            $modal->assertSee('Berhasil Menghapus Bank!');
-               
-                        });
+        ->visit('/dashboard#/bank')
+        ->assertSeeLink('Tambah Bank')
+        ->whenAvailable('.data-ada', function ($modal) use ($bank) {
+          $modal->click('#delete-'.$bank->id) 
+          ->assertDialogOpened('Yakin Ingin Menghapus Bank MANDIRI SYARIAH ?');
+
+        })
+        ->driver->switchTo()->alert()->accept();
+        $masterBank->whenAvailable('.swal-modal', function ($modal) {
+          $modal->assertSee('Berhasil Menghapus Bank!');
+
         });
+      });
 
     } 
 
-        public function testUniqueBank() {
-        $this->browse(function ($masterBank, $second) {
-           $masterBank->loginAs(User::find(1))
-                      ->visit('/bank')
-                      ->clickLink('Tambah Bank')
-                      ->type('nama_bank','BNI SYARIAH')
-                      ->type('atas_nama','Afif Maulana')
-                      ->type('no_rek','784596123');
-          $masterBank->script("document.getElementById('tampil_customer').checked = true;"); 
-        $masterBank->press('#btnSimpanBank');                      
-        $masterBank->script('document.getElementById("no_rek").focus();');
-        $masterBank->assertSeeIn('#no_rek_error','Maaf no rek Sudah Terpakai');
-        });
+    public function testUniqueBank() {
+      $this->browse(function ($masterBank, $second) {
+       $masterBank->loginAs(User::find(1))
+       ->visit('/dashboard#/bank')
+       ->clickLink('Tambah Bank')
+       ->type('nama_bank','BNI SYARIAH')
+       ->type('atas_nama','Afif Maulana')
+       ->type('no_rek','784596123');
+       $masterBank->script("document.getElementById('tampil_customer').checked = true;"); 
+       $masterBank->press('#btnSimpanBank');                      
+       $masterBank->assertSee('MAAF NO REK SUDAH TERPAKAI.');
+     });
     }
 
-  
-}
+
+  }
