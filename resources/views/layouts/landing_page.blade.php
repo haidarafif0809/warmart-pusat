3c19c18a3192ff25658e25a702fc511ac4dba490
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2017-11-10 10:15:22 +0700

    Instalasi TawkTo untuk chatting customer

--- resources/views/layouts/landing_page.blade.php
+++ resources/views/layouts/landing_page.blade.php
@@ -444,8 +444,20 @@
 </div>
 </footer>
 
-
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5a051374bb0c3f433d4c84cd/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+</body>
 <!--   Core JS Files   -->
 <script src="{{ asset('js/jquery-3.2.1.min.js') }}" type="text/javascript"></script>
 <script src="{{ asset('js/bootstrap.min.js') }}" type="text/javascript"></script>
@@ -502,7 +514,5 @@
     
     $(".carousel").carousel({interval: 1000 * 10 });
 </script>
-
-scrip
 
 </html>
