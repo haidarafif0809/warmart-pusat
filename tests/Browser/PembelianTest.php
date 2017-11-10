0b4520dff0ca47f989d409c7c0d4b115c7db28f4
Riko Pernando <rikopernando96@gmail.com>
2017-11-10 16:14:22 +0700

    Browser Test Pembelian
    Browser Test Pembelian

--- tests/Browser/PembelianTest.php
+++ tests/Browser/PembelianTest.php
@@ -7,6 +7,7 @@ use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Auth;
 use App\User;
+use App\Pembelian;
 
 class PembelianTest extends DuskTestCase
 {
@@ -226,6 +227,104 @@ class PembelianTest extends DuskTestCase
             ->press('Submit')
             ->assertSee('BERHASIL MENGUBAH PAJAK PRODUK "KECAP ASIN ABC"')
             ->assertSeeIn('#table-subtotal','56000');
+        }); 
+    }
+
+
+    public function testPajakTerlaluBesarTbsPembelian(){
+        $this->browse(function ($first, $second)  {
+            $first->loginAs(User::find(5))
+            ->visit('/pembelian')                  
+            ->clickLink('Tambah Pembelian')
+            ->assertSee('Pembelian');
+
+            $first->script("document.getElementById('pilih_produk').selectize.setValue('1-KECAP ASIN ABC-55000');");
+            $first->assertSee('B001 - KECAP ASIN ABC')
+            ->pause(1000);
+
+            $first->type('#jadwal_produk_swal','1')
+            ->press('Submit')
+            ->assertSee('BERHASIL MENAMBAH PRODUK "KECAP ASIN ABC"')
+
+            ->press("#edit_tax_produk")
+            ->assertSee('Sertakan % Jika Ingin Pajak Dalam Bentuk Persentase')
+            ->select('#ppn_swal','Exclude')
+            ->assertSeeIn('#ppn_swal','Exclude')
+            ->type('#tax_swal','100000')
+            ->press('Submit')
+            ->assertSee('Pajak Tidak Boleh Melebihi Subtotal!')
+            ->press("OK")
+            ->pause(1000)
+
+            ->press("#edit_tax_produk")
+            ->assertSee('Sertakan % Jika Ingin Pajak Dalam Bentuk Persentase')
+            ->select('#ppn_swal','Exclude')
+            ->assertSeeIn('#ppn_swal','Exclude')
+            ->type('#tax_swal','100000%')
+            ->press('Submit')
+            ->assertSee('Pajak Tidak Boleh Lebih Dari 100%!')
+            ->press("OK");
+        }); 
+    }
+
+
+
+    public function testSelesaiTransaksiPembelianTunai(){
+
+        $no_faktur = Pembelian::no_faktur(1); 
+
+        $this->browse(function ($first, $second) use ($no_faktur) {
+            $first->loginAs(User::find(5))
+            ->visit('/pembelian')                  
+            ->clickLink('Tambah Pembelian')
+            ->assertSee('Pembelian');
+
+            $first->script("document.getElementById('pilih_produk').selectize.setValue('1-KECAP ASIN ABC-55000');");
+            $first->assertSee('B001 - KECAP ASIN ABC')
+            ->pause(1000);
+
+            $first->type('#jadwal_produk_swal','1')
+            ->press('Submit')
+            ->assertSee('BERHASIL MENAMBAH PRODUK "KECAP ASIN ABC"');
+
+            $first->pause(1000)
+            ->press('SELESAI (F2)')
+            ->assertDialogOpened('Suplier Belum Dipilih!');
+            $first->driver->switchTo()->alert()->accept();
+
+            $first->script("document.getElementById('pilih_suplier').selectize.setValue('1');");
+            $first->assertSee("JAYA BAROKAH")
+            ->press('SELESAI (F2)')
+            ->pause(1000);
+
+            $first->whenAvailable('.modal', function ($modal) use($no_faktur){
+                $modal->assertSee('Silahkan Lengkapi Pembayaran!');
+                $modal->assertInputValue('#subtotal','55.000,00')
+                ->assertInputValue('#total_akhir','55.000,00')
+                ->assertInputValue('#kredit','55.000,00')
+
+                ->type('pembayaran','100000')
+                ->assertInputValue('#kembalian','45.000,00')
+                ->assertInputValue('#kredit','')
+
+                ->type('potongan_persen','10')
+                ->assertInputValue('#potongan_faktur','5500')
+                ->assertInputValue('#total_akhir','49.500,00')
+                ->assertInputValue('#kembalian','50.500,00')
+                ->assertInputValue('#kredit','')
+
+                ->type('potongan_faktur','10000')
+                ->assertInputValue('#total_akhir','45.000,00')
+                ->assertInputValue('#kembalian','55.000,00')
+                ->assertInputValue('#kredit','')
+                ->type('keterangan','Browser Test Pembelian')
+                ->press('TUNAI');
+            });
+            $first->pause(1000)
+            ->assertSee('SUKSES : BERHASIL MELAKUKAN TRANSAKSI PEMBELIAN FAKTUR "'.$no_faktur.'"');
+
+
+
         }); 
     }
 
