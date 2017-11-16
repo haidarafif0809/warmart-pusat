4972ebe1df227f38736a6a26267c37549acd38a9
RindangRamadhan <rindangramadhan10@gmail.com>
2017-11-16 13:48:35 +0700

    TestCase Master Data Customer
    Input tanggan masih manual dan klik Ok konfirmasi delete

--- tests/Browser/CustomerTest.php
+++ tests/Browser/CustomerTest.php
@@ -22,72 +22,22 @@ class CustomerTest extends DuskTestCase
        $CustomerTest->loginAs(User::find(1))
        ->visit('/dashboard#/customer')
        ->assertSee('Customer')
+       ->pause('2000')
        ->clickLink('Tambah Customer')
        ->type('name','Ridho Ficardo')
        ->type('no_telp','4')
        ->type('email','ridho@gmail.com')
        ->type('alamat','Jl. Kemiling');
-       $CustomerTest->script(["document.getElementById('tanggal_lahir').value = '10 Jan 1997' ",]);
+       $CustomerTest->script(["document.getElementById('tanggal_lahir').value = '10 Mar 1996' ",]);
        $CustomerTest->script("document.getElementById('komunitas').selectize.setValue('2');");
        $CustomerTest->assertSee('Sample Member');
        $CustomerTest->press("#btnSimpanCustomer")
+       ->waitFor('.swal-modal', 10)
        ->whenAvailable('.swal-modal', function ($modal) {
-        $modal->assertSee('Berhasil Mengubah Menambah Ridho Ficardo')
+        $modal->assertSee('Berhasil Menambah Customer Ridho Ficardo')
         ->press('OK');
       });
      });
-    }
-
-
-    public function testUbahCustomer(){
-
-      $customer = Customer::select('id')->where('no_telp','4')->first();
-      $this->browse(function ($customerTest) use($customer){
-        $customerTest->loginAs(User::find(1))
-        ->visit('/dashboard#/customer')
-        ->assertSee('Customer')
-        ->with('.data-ada', function ($table) use($customer){
-          $table->assertSee('Ridho Ficardo')
-          ->press('#edit-'.$customer->id);
-        })
-        ->assertSee('Edit Customer')
-        ->type('name','Ficardo Ridho')
-        ->type('no_telp','3')
-        ->type('email','ficardo@gmail.com')
-        ->type('alamat','Jl. Kedaton');
-        $customerTest->script(["document.getElementById('tanggal_lahir').value = '10 Mar 1996' ",]);
-        $customerTest->script("document.getElementById('komunitas').selectize.setValue('2');");  
-        $customerTest->assertSee('Sample Member');
-        $customerTest->press("#btnSimpanCustomer")
-        ->whenAvailable('.swal-modal', function ($modal) {
-          $modal->assertSee('Berhasil Mengubah customer Ficardo Ridho')
-          ->press('OK');
-        });        
-      });
-    }
-
-    public function testHapusCustomer() {
-
-      $customer = Customer::select('id')->where('no_telp','4')->first();
-
-      $this->browse(function ($customerTest)use($customer) {
-        $customerTest->loginAs(User::find(1))
-        ->visit('/dashboard#/customer')
-        ->assertSee('Customer')
-        ->with('.data-ada', function ($table) use($customer){
-          $table->assertSee('Ficardo Ridho')
-          ->press('#delete-'.$customer->id)
-          ->whenAvailable('.swal-modal', function ($modal) {
-            $modal->assertSee('Anda Yakin Ingin Menghapus Customer Ficardo Ridho ?')
-            ->press('OK')
-            ->whenAvailable('.swal-modal', function ($modal_hapus) {
-              $modal_hapus->assertSee('Customer Berhasil Dihapus!')
-              ->press('OK');
-            });
-          });        
-        });
-      });
-
     }
 
     public function testUniqueEmailCustomer() {
@@ -103,9 +53,39 @@ class CustomerTest extends DuskTestCase
        $CustomerTest->script(["document.getElementById('tanggal_lahir').value = '10 Mar 1996' ",]);
        $CustomerTest->script("document.getElementById('komunitas').selectize.setValue('2');");  
        $CustomerTest->assertSee('Sample Member');
-       $CustomerTest->press("#btnSimpanCustomer");
-       $CustomerTest->assertSeeIn('#no_telp_error','MAAF NO TELP SUDAH TERPAKAI.');
+       $CustomerTest->press("#btnSimpanCustomer")
+       ->whenAvailable('#no_telp_error', function ($error_message){
+        $error_message->assertSee('MAAF NO TELP SUDAH TERPAKAI.');
+      });
      });
+    }
+
+    public function testUbahCustomer(){
+
+      $customer = Customer::select('id')->where('no_telp','4')->first();
+      $this->browse(function ($customerTest) use($customer){
+        $customerTest->loginAs(User::find(1))
+        ->visit('/dashboard#/customer')
+        ->assertSee('Customer')
+        ->pause('2000')
+        ->with('.data-ada', function ($table) use($customer){
+          $table->assertSee('Ridho Ficardo')
+          ->press('#edit-'.$customer->id);
+        })
+        ->assertSee('Edit Customer')
+        ->type('name','Ficardo Ridho')
+        ->type('no_telp','4')
+        ->type('email','ficardo@gmail.com')
+        ->type('alamat','Jl. Kedaton');
+        $customerTest->script(["document.getElementById('tanggal_lahir').value = '10 Mar 1996' ",]);
+        $customerTest->script("document.getElementById('komunitas').selectize.setValue('2');");  
+        $customerTest->assertSee('Sample Member');
+        $customerTest->press("#btnSimpanCustomer")
+        ->whenAvailable('.swal-modal', function ($modal) {
+          $modal->assertSee('Berhasil Mengubah customer Ficardo Ridho')
+          ->press('OK');
+        });        
+      });
     }
 
     public function testDetailCustomer(){
@@ -115,6 +95,7 @@ class CustomerTest extends DuskTestCase
         $customerTest->loginAs(User::find(1))
         ->visit('/dashboard#/customer')
         ->assertSee('Customer')
+        ->pause('2000')
         ->with('.data-ada', function ($table) use($customer){
           $table->assertSee('Ficardo Ridho')
           ->press('#detail-'.$customer->id);
@@ -122,6 +103,35 @@ class CustomerTest extends DuskTestCase
         ->assertSee('Detail Customer');     
       });
     }
-    
+
+    public function testHapusCustomer() {
+
+      $customer = Customer::select('id')->where('no_telp','4')->first();
+
+      $this->browse(function ($customerTest)use($customer) {
+        $customerTest->loginAs(User::find(1))
+        ->visit('/dashboard#/customer')
+        ->assertSee('Customer')
+        ->pause('2000')
+        ->with('.data-ada', function ($table) use($customer){
+          $table->assertSee('Ficardo Ridho')
+          ->press('#delete-'.$customer->id)
+          ->whenAvailable('.swal-modal', function ($modal) {
+            $modal->assertSee('Anda Yakin Ingin Menghapus Customer Ficardo Ridho ?')
+            ->press('OK')
+            ->waitFor('.swal-modal', 10)
+            ->whenAvailable('.swal-modal', function ($modal_hapus) {
+              $modal_hapus->assertSee('Customer Berhasil Dihapus!')
+              ->press('OK')
+              ->whenAvailable('.swal-footer', function ($tombol_konfirm) {
+                $tombol_konfirm->assertSee('Customer Berhasil Dihapus!')
+                ->press('OK');
+              });
+            });
+          });        
+        });
+      });
+
+    }    
 
   }
