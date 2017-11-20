f1a336a19522f7560bd5cf7ca9dc160aa21ccbbc
Unknown <ghozam.alghifari96@gmail.com>
2017-11-20 16:34:28 +0700

    Membuat Pesanan Pelanggan

---
+++ resources/views/layouts/pesanan_pelanggan.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.app_pelanggan')
+@section('content') 
+
+@if (Agent::isMobile()) <!--JIKA DAKSES VIA HP/TAB-->
+
+<div class="page-header header-small" data-parallax="true"" style="background-image: url('./image/background2.jpg');"> 
+  <div class="container"> 
+    <div class="row">
+      <div class="col-md-8 col-md-offset-2">
+        <div class="brand">
+          <h3 class="title">PASAR MUSLIM INDONESIA</h3>
+          <h6 class="title"> Segala Kemudahan Untuk Umat Muslim Berbelanja.</h6>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@else <!--JIKA DIAKSES VIA KOMPUTER-->
+<div class="page-header header-filter header-small" data-parallax="true" style="background-image: url('./image/background2.jpg');"> 
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 col-md-offset-2">
+        <div class="brand">
+          <h1 class="title">PASAR MUSLIM INDONESIA</h1>
+          <h4 class="title"> Segala Kemudahan Untuk Umat Muslim Berbelanja.</h4>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+@endif
+
+
+
+
+<div class="main main-raised"> 
+  <div class="container">  
+    <ul class="breadcrumb" style="margin-top: 10px">
+      <li><a href="{{ url('/daftar-produk') }}">Home</a></li>
+      <li class="active">Pesanan</li>
+    </ul>    
+    <div class="card-content"> 
+      <h3 class="title text-center">Pesanan</h3> 
+
+      @if (Agent::isMobile()) <!--JIKA DAKSES VIA HP/TAB-->
+      {!! $produk_pesanan_mobile !!}
+
+      @else <!--JIKA DIAKSES VIA KOMPUTER-->
+      <div class="card">
+        <div class="col-md-12">
+
+          <div class="content">
+            <div class="container-fluid">
+              <div class="row"> 
+                <div class="col-md-12">
+                  <div class="card card-plain">
+                    <div class="card-header card-header-icon" data-background-color="rose"> 
+                    </div> 
+                    <div class="card-content">
+                      <table class="table table-hover table-responsive">
+                        <thead> 
+                          <th>Pesanan</th>
+                          <th>Di pesan pada </th>
+                          <th>Total</th>
+                          <th>Status</th>
+                        </thead>
+                        <tbody>
+                          {!! $produk_pesanan_komputer !!}
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>  
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      @endif
+    </div> <!-- end-main-raised --> 
+  </div>
+</div> 
+</div> 
+@endsection
+
+@section('scripts')   
+@endsection 
