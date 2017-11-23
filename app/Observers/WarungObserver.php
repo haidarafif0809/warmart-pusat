6e4b2cfae5d181972336f952d43302c814f67d3a
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2017-11-23 13:48:00 +0700

    ketika warung di hapus maka barangnya juga di hapus

--- app/Observers/WarungObserver.php
+++ app/Observers/WarungObserver.php
@@ -6,16 +6,18 @@ use App\Warung;
 use App\User;
 use App\BankWarung;
 use Session;
+use App\Barang;
 
 class WarungObserver
 {
-    public function deleting(Warung $Warung)
-    {       
+	public function deleting(Warung $Warung)
+	{       
 
-        BankWarung::where('warung_id', $Warung->id)->delete();
-        User::where('id_warung', $Warung->id)->delete();
-        return true;
+		BankWarung::where('warung_id', $Warung->id)->delete();
+		Barang::where('id_warung',$Warung->id)->delete();
+		User::where('id_warung', $Warung->id)->delete();
+		return true;
 
-    }
+	}
 
 }
