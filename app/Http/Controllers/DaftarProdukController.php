5ca10a1ad8d1e814661d3ecbc814669f6d1cd0bf
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2017-11-28 10:14:00 +0700

    card warung hapus dulu flexfontnya

--- app/Http/Controllers/DaftarProdukController.php
+++ app/Http/Controllers/DaftarProdukController.php
@@ -453,13 +453,13 @@ public static function cardWarung($warungs){
   </p>
   </div>
   <div class="col-md-9 col-sm-9 col-xs-9">              
-  <p class="flexFontWarung">
+  <p class="">
   <a href="halaman-warung/'.$warungs->id.'" >';
   $card_warung .= DaftarProdukController::warungNama($warungs);
   $card_warung .= '
   </a>
   </p>
-  <p class="flexFontWarung">            
+  <p class="">            
   <a href="halaman-warung/'.$warungs->id.'">';
   $card_warung .= DaftarProdukController::alamatWarung($warungs);
   $card_warung .= '
