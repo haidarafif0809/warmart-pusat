7528035adba62070e78d519480d8adccf80cf679
Riko Pernando <rikopernando96@gmail.com>
2017-11-30 16:31:56 +0700

    Proses Pesanan Pelanggan Bisa Lebih dari 1 warung dan Querynya Dibuatkan Scope
    Proses Pesanan Pelanggan Bisa Lebih dari 1 warung dan Querynya Dibuatkan Scope

--- app/Http/Controllers/PemesananController.php
+++ app/Http/Controllers/PemesananController.php
@@ -2,125 +2,170 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Session;  
-use SEOMeta;
-use OpenGraph;
-use Twitter;
-use App\KeranjangBelanja; 
-use App\Barang;
+use App\DetailPesananPelanggan;
+use App\KeranjangBelanja;
+use App\PesananPelanggan;
 use App\Warung;
-use App\Hpp;  
-use App\PesananPelanggan;  
-use App\DetailPesananPelanggan;  
-use Jenssegers\Agent\Agent;
-use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\Client;
 use Auth;
 use DB;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use Jenssegers\Agent\Agent;
+use OpenGraph;
+use SEOMeta;
 
 class PemesananController extends Controller
-{ 
+{
 
-	public function selesaikanPemesanan()
-	{ 
-		SEOMeta::setTitle('War-Mart.id');
-		SEOMeta::setDescription('Warmart marketplace warung muslim pertama di Indonesia');
-		SEOMeta::setCanonical('https://war-mart.id');
-		SEOMeta::addKeyword(['warmart', 'warung', 'marketplace','toko online','belanja','lazada']);
+    public function selesaikanPemesanan()
+    {
+        SEOMeta::setTitle('War-Mart.id');
+        SEOMeta::setDescription('Warmart marketplace warung muslim pertama di Indonesia');
+        SEOMeta::setCanonical('https://war-mart.id');
+        SEOMeta::addKeyword(['warmart', 'warung', 'marketplace', 'toko online', 'belanja', 'lazada']);
 
-		OpenGraph::setDescription('Warmart marketplace warung muslim pertama di Indonesia');
-		OpenGraph::setTitle('War-Mart.id');
-		OpenGraph::setUrl('https://war-mart.id');
-		OpenGraph::addProperty('type', 'articles'); 
+        OpenGraph::setDescription('Warmart marketplace warung muslim pertama di Indonesia');
+        OpenGraph::setTitle('War-Mart.id');
+        OpenGraph::setUrl('https://war-mart.id');
+        OpenGraph::addProperty('type', 'articles');
 
-		$agent = new Agent();
+        $agent = new Agent();
 
-		$keranjang_belanjaan = KeranjangBelanja::with(['produk','pelanggan'])->where('id_pelanggan',Auth::user()->id)->get();
-		$cek_belanjaan = $keranjang_belanjaan->count();  
+        $keranjang_belanjaan = KeranjangBelanja::with(['produk', 'pelanggan'])->where('id_pelanggan', Auth::user()->id)->get();
+        $cek_belanjaan       = $keranjang_belanjaan->count();
 
-		$jumlah_produk = KeranjangBelanja::select([DB::raw('IFNULL(SUM(jumlah_produk),0) as total_produk')])->where('id_pelanggan',Auth::user()->id)->first();  
-		//FOTO WARMART
-		$logo_warmart = "".asset('/assets/img/examples/warmart_logo.png')."";
+        $jumlah_produk = KeranjangBelanja::select([DB::raw('IFNULL(SUM(jumlah_produk),0) as total_produk')])->where('id_pelanggan', Auth::user()->id)->first();
+        //FOTO WARMART
+        $logo_warmart = "" . asset('/assets/img/examples/warmart_logo.png') . "";
 
-		$subtotal = 0; 
-		foreach ($keranjang_belanjaan as $keranjang_belanjaans) {   
-			$harga_produk = $keranjang_belanjaans->produk->harga_jual * $keranjang_belanjaans->jumlah_produk;  
-			$subtotal = $subtotal += $harga_produk;  
-		}  
+        $subtotal = 0;
+        foreach ($keranjang_belanjaan as $keranjang_belanjaans) {
+            $harga_produk = $keranjang_belanjaans->produk->harga_jual * $keranjang_belanjaans->jumlah_produk;
+            $subtotal     = $subtotal += $harga_produk;
+        }
 
-		$user = Auth::user();
+        $user = Auth::user();
 
-		return view('layouts.selesaikan_pemesanan',['keranjang_belanjaan'=>$keranjang_belanjaan,'cek_belanjaan'=>$cek_belanjaan,'agent'=>$agent,'jumlah_produk'=>$jumlah_produk,'logo_warmart'=>$logo_warmart,'subtotal'=>$subtotal,'user'=>$user]);
-	}
+        return view('layouts.selesaikan_pemesanan', ['keranjang_belanjaan' => $keranjang_belanjaan, 'cek_belanjaan' => $cek_belanjaan, 'agent' => $agent, 'jumlah_produk' => $jumlah_produk, 'logo_warmart' => $logo_warmart, 'subtotal' => $subtotal, 'user' => $user]);
+    }
 
+    public function prosesSelesaikanPemesanan(Request $request)
+    {
+        //START TRANSAKSI
+        DB::beginTransaction();
 
+        // QUERY LENGKAPNYA ADA DI scopeKeranjangBelanjaPelanggan di model Keranjang Belanja
+        $keranjang_belanjaan = KeranjangBelanja::KeranjangBelanjaPelanggan()->get();
 
-	public function prosesSelesaikanPemesanan(Request $request)
-	{  
+        $id_user = Auth::user()->id;
 
-		$keranjang_belanjaan = KeranjangBelanja::select('keranjang_belanjas.id_keranjang_belanja AS id_keranjang_belanja','keranjang_belanjas.id_produk AS id_produk','keranjang_belanjas.jumlah_produk AS jumlah_produk','barangs.harga_jual AS harga_jual','barangs.id_warung AS id_warung')
-		->leftJoin('barangs','keranjang_belanjas.id_produk','=','barangs.id')
-		->where('id_pelanggan',Auth::user()->id);
+        $cek_pesanan = 0; // BUAT VARIABEL CEK PESANAN YANG KITA SET  0
+        foreach ($keranjang_belanjaan as $key => $keranjang_belanjaans) {
 
-		$keranjang = $keranjang_belanjaan->get(); 
+            $id_warung = $keranjang_belanjaans['id_warung'];
 
-		$id_warung = $keranjang_belanjaan->first()->id_warung; 
-		$id_user = Auth::user()->id;
+            // $key adalah urutan perulangan di foreach
 
-		$pesanan_pelanggan = PesananPelanggan::create([
-			'id_pelanggan'     => $id_user,      
-			'nama_pemesan'     => $request->name,              
-			'no_telp_pemesan'     => $request->no_telp,
-			'alamat_pemesan' => $request->alamat,
-			'jumlah_produk'     => $request->jumlah_produk,
-			'subtotal'     => $request->subtotal,
-			'id_warung' 	=> $id_warung
-		]);   
+            // jika perulangan yg pertama maka proses dibawah akan dijalan kan
+            if ($key == 0) {
 
-		$id_pesanan_pelanggan =  $pesanan_pelanggan->id;
+                // QUERY LENGKAPMNYA ADA DI scopeHitungTotalPesanan di mmodel Keranjang Belanja
+                $query_hitung_total = KeranjangBelanja::HitungTotalPesanan($id_warung)->first();
 
+                // INSERT KE PESANAN PELANGGAN
+                $pesanan_pelanggan = PesananPelanggan::create([
+                    'id_pelanggan'    => $id_user,
+                    'nama_pemesan'    => $request->name,
+                    'no_telp_pemesan' => $request->no_telp,
+                    'alamat_pemesan'  => $request->alamat,
+                    'jumlah_produk'   => $query_hitung_total['total_produk'],
+                    'subtotal'        => $query_hitung_total['total_pesanan'],
+                    'id_warung'       => $id_warung,
+                ]);
 
-		$warung = Warung::find($id_warung);
+                // UBAH NILAI VARIABEL CEK PESANAN JADI ID WARUNG
+                $cek_pesanan = $id_warung;
 
-		$nomor_tujuan = $warung->no_telpon;
+                // ID PESANAN PELANGGAN
+                $id_pesanan_pelanggan = $pesanan_pelanggan->id;
 
+                // SELECT WARUNG
+                $warung = Warung::find($id_warung);
 
-		$this->kirimSmsKeWarung($nomor_tujuan,$id_pesanan_pelanggan);
+                // AMBIL NOMOR TELPON WARUNG
+                $nomor_tujuan = $warung->no_telpon;
 
-		foreach ($keranjang as $keranjang_belanjaans) {  
+                // KIRIM SMS KE WARUNG
+                $this->kirimSmsKeWarung($nomor_tujuan, $id_pesanan_pelanggan);
+            }
 
-			DetailPesananPelanggan::create([
-				'id_pesanan_pelanggan'     => $pesanan_pelanggan->id,              
-				'id_produk'     => $keranjang_belanjaans['id_produk'],
-				'id_pelanggan' => $id_user,
-				'harga_produk' => $keranjang_belanjaans['harga_jual'],
-				'jumlah_produk'     => $keranjang_belanjaans['jumlah_produk'], 
-			]);   
+            // JIKA CEK PESANAN TIDAK SAMA DENGAN NOL DAN CEK PESANAN TIDAK SAMA DENGAN ID WARUNG
+            if ($cek_pesanan != 0 and $cek_pesanan != $id_warung) {
 
-			KeranjangBelanja::destroy($keranjang_belanjaans['id_keranjang_belanja']);
+                // QUERY LENGKAPMNYA ADA DI scopeHitungTotalPesanan di mmodel Keranjang Belanja
+                $query_hitung_total = KeranjangBelanja::HitungTotalPesanan($id_warung)->first();
 
-		}
+                // INSERT PESANAN PELANGGAN
+                $pesanan_pelanggan = PesananPelanggan::create([
+                    'id_pelanggan'    => $id_user,
+                    'nama_pemesan'    => $request->name,
+                    'no_telp_pemesan' => $request->no_telp,
+                    'alamat_pemesan'  => $request->alamat,
+                    'jumlah_produk'   => $query_hitung_total['total_produk'],
+                    'subtotal'        => $query_hitung_total['total_pesanan'],
+                    'id_warung'       => $id_warung,
+                ]);
 
+                // UBAH NILAI VARIABEL CEK PESANAN JADI ID WARUNG
+                $cek_pesanan = $id_warung;
 
-		return redirect()->route('daftar_produk.index');
+                // ID PESANAN PELANGGAN
+                $id_pesanan_pelanggan = $pesanan_pelanggan->id;
 
-	}
+                // SELECT WARUNG
+                $warung = Warung::find($id_warung);
 
-	public function kirimSmsKeWarung($nomor_tujuan,$id_pesanan_pelanggan){
+                // AMBIL NOMOR TELPON WARUNG
+                $nomor_tujuan = $warung->no_telpon;
 
-
-
-		$userkey = env('USERKEY');
-		$passkey = env('PASSKEY');
-		$isi_pesan = urlencode('Warmart: Assalamualaikum, Ada Pesanan baru Silakan Cek war-mart.id/detail-pesanan-warung/'.$id_pesanan_pelanggan);
-		
-		if (env('STATUS_SMS') == 1) {
-                $client = new Client(); //GuzzleHttp\Client
-                $result = $client->get('https://reguler.zenziva.net/apps/smsapi.php?userkey='.$userkey.'&passkey='.$passkey.'&nohp='.$nomor_tujuan.'&pesan='.$isi_pesan.''); 
+                // KIRIM SMS KE WARUNG
+                $this->kirimSmsKeWarung($nomor_tujuan, $id_pesanan_pelanggan);
 
             }
+
+            // INSERT KE DETAIL PESANAN PELANGGAN
+            DetailPesananPelanggan::create([
+                'id_pesanan_pelanggan' => $pesanan_pelanggan->id,
+                'id_produk'            => $keranjang_belanjaans['id_produk'],
+                'id_pelanggan'         => $id_user,
+                'harga_produk'         => $keranjang_belanjaans['harga_jual'],
+                'jumlah_produk'        => $keranjang_belanjaans['jumlah_produk'],
+            ]);
+
+            // HAPUS KERANJANG BELANJA
+            KeranjangBelanja::destroy($keranjang_belanjaans['id_keranjang_belanja']);
+
+        }
+
+        DB::commit();
+
+        return redirect()->route('daftar_produk.index');
+
+    }
+
+    public function kirimSmsKeWarung($nomor_tujuan, $id_pesanan_pelanggan)
+    {
+
+        $userkey   = env('USERKEY');
+        $passkey   = env('PASSKEY');
+        $isi_pesan = urlencode('Warmart: Assalamualaikum, Ada Pesanan baru Silakan Cek war-mart.id/detail-pesanan-warung/' . $id_pesanan_pelanggan);
+
+        if (env('STATUS_SMS') == 1) {
+            $client = new Client(); //GuzzleHttp\Client
+            $result = $client->get('https://reguler.zenziva.net/apps/smsapi.php?userkey=' . $userkey . '&passkey=' . $passkey . '&nohp=' . $nomor_tujuan . '&pesan=' . $isi_pesan . '');
 
         }
 
     }
+
+}
--- app/KeranjangBelanja.php
+++ app/KeranjangBelanja.php
@@ -2,46 +2,70 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
 use Auth;
+use DB;
+use Illuminate\Database\Eloquent\Model;
 
 class KeranjangBelanja extends Model
 {
     //
-	protected $fillable = ['id_produk','id_pelanggan','jumlah_produk'];
-	protected $primaryKey = 'id_keranjang_belanja';
-      // relasi ke produk
-	public function produk(){
-		return $this->hasOne('App\Barang','id','id_produk');
-	} 
-      // relasi ke pelanggan
-	public function pelanggan(){
-		return $this->hasOne('App\User','id','id_pelanggan');
-	} 
+    protected $fillable   = ['id_produk', 'id_pelanggan', 'jumlah_produk'];
+    protected $primaryKey = 'id_keranjang_belanja';
+    // relasi ke produk
+    public function produk()
+    {
+        return $this->hasOne('App\Barang', 'id', 'id_produk');
+    }
+    // relasi ke pelanggan
+    public function pelanggan()
+    {
+        return $this->hasOne('App\User', 'id', 'id_pelanggan');
+    }
 
-	public function scopeJumlahBelanja($query){
-		return $query->where('id_pelanggan',Auth::user()->id)->count();
-	}
+    public function scopeJumlahBelanja($query)
+    {
+        return $query->where('id_pelanggan', Auth::user()->id)->count();
+    }
 
-	public function getNamaProdukAttribute() 
-	{   
-		return title_case($this->produk->nama_barang); 
+    public function getNamaProdukAttribute()
+    {
+        return title_case($this->produk->nama_barang);
 
-	}
+    }
 
+    public function getNamaProdukMobileAttribute()
+    {
+        if (strlen(strip_tags($this->produk->nama_barang)) <= 33) {
 
-	public function getNamaProdukMobileAttribute() 
-	{   
-		if (strlen(strip_tags($this->produk->nama_barang)) <= 33) {
+            $nama_produk = title_case(strip_tags($this->produk->nama_barang));
+        } else {
 
-			$nama_produk =title_case( strip_tags($this->produk->nama_barang));
-		}else{
+            $nama_produk = title_case('' . strip_tags(substr($this->produk->nama_barang, 0, 30)) . '...');
+        }
 
-			$nama_produk = title_case(''.strip_tags(substr($this->produk->nama_barang, 0, 30)).'...'); 
-		}
+        return $nama_produk;
+    }
 
-		return $nama_produk;
-	}
-	
+    public function scopeKeranjangBelanjaPelanggan($query)
+    {
 
+        $query->select('keranjang_belanjas.id_keranjang_belanja AS id_keranjang_belanja', 'keranjang_belanjas.id_produk AS id_produk', 'keranjang_belanjas.jumlah_produk AS jumlah_produk', 'barangs.harga_jual AS harga_jual', 'barangs.id_warung AS id_warung')
+            ->leftJoin('barangs', 'keranjang_belanjas.id_produk', '=', 'barangs.id')
+            ->where('id_pelanggan', Auth::user()->id)->orderBy('barangs.id_warung');
+
+        return $query;
+
+    }
+
+    public function scopeHitungTotalPesanan($query, $id_warung)
+    {
+
+        $query->select(DB::raw('SUM(keranjang_belanjas.jumlah_produk) as total_produk'), DB::raw('SUM(keranjang_belanjas.jumlah_produk * barangs.harga_jual) as total_pesanan'))
+            ->leftJoin('barangs', 'keranjang_belanjas.id_produk', '=', 'barangs.id')
+            ->where('id_pelanggan', Auth::user()->id)
+            ->where('barangs.id_warung', $id_warung);
+
+        return $query;
+
+    }
 }
