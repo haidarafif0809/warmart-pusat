77c8c7fde01fb2873225072fc6966528ce66b8f1
RindangRamadhan <rindangramadhan10@gmail.com>
2017-12-05 10:10:52 +0700

    Ubah versi app.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -409,7 +409,7 @@
 <!--End of Tawk.to Script-->
 </body>
 <!--   Core JS Files   -->
-<script type="text/javascript" src="{{ asset('js/app.js?v=1.20')}}"></script>
+<script type="text/javascript" src="{{ asset('js/app.js?v=1.21')}}"></script>
 <script src="{{ asset('js/bootstrap.min.js') }}" type="text/javascript"></script>
 <script src="{{ asset('js/material.min.js') }}" type="text/javascript"></script>
 
