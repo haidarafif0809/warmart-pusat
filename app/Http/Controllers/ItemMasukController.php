b1204afa784281675bed121242b64af50a20fb65
Riko Pernando <rikopernando96@gmail.com>
2017-12-09 09:33:03 +0700

    Conflict
    Conflict

--- app/Http/Controllers/ItemMasukController.php
+++ app/Http/Controllers/ItemMasukController.php
@@ -105,14 +105,15 @@ class ItemMasukController extends Controller
     {
         $session_id     = session()->getId();
         $user_warung    = Auth::user()->id_warung;
-        $tbs_item_masuk = TbsItemMasuk::with(['produk'])->where('warung_id', $user_warung)->where('session_id', $session_id)->paginate(10);
+        $tbs_item_masuk = TbsItemMasuk::with(['produk'])->where('warung_id', $user_warung)->where('session_id', $session_id)->orderBy('id_tbs_item_masuk', 'desc')->paginate(10);
         $array          = array();
 
         foreach ($tbs_item_masuk as $tbs_item_masuks) {
             array_push($array, [
-                'id'          => $tbs_item_masuks->id,
-                'nama_produk' => $tbs_item_masuks->nama_barang,
-                'kode_produk' => $tbs_item_masuks->kode_barang]);
+                'id_tbs_item_masuk' => $tbs_item_masuks->id_tbs_item_masuk,
+                'nama_produk'       => $tbs_item_masuks->TitleCaseProduk,
+                'kode_produk'       => $tbs_item_masuks->produk->kode_barang,
+                'jumlah_produk'     => $tbs_item_masuks->jumlah_produk]);
         }
 
         //DATA PAGINATION
@@ -142,14 +143,15 @@ class ItemMasukController extends Controller
                 $query->orWhere('id_produk', 'LIKE', $request->search . '%')
                     ->orWhere('jumlah_produk', 'LIKE', $request->search . '%');
 
-            })->orderBy('id', 'desc')->paginate(10);
+            })->orderBy('id_tbs_item_masuk', 'desc')->paginate(10);
 
         $array = array();
         foreach ($tbs_item_masuk as $tbs_item_masuks) {
             array_push($array, [
-                'id'          => $tbs_item_masuks->id,
-                'nama_produk' => $tbs_item_masuks->produk->nama_barang,
-                'kode_produk' => $tbs_item_masuks->produk->kode_barang]);
+                'id_tbs_item_masuk' => $tbs_item_masuks->id_tbs_item_masuk,
+                'nama_produk'       => $tbs_item_masuks->TitleCaseProduk,
+                'kode_produk'       => $tbs_item_masuks->produk->kode_barang,
+                'jumlah_produk'     => $tbs_item_masuks->jumlah_produk]);
         }
 
         //DATA PAGINATION
@@ -208,63 +210,29 @@ class ItemMasukController extends Controller
     //PROSES TAMBAH TBS ITEM MASUK
     public function proses_tambah_tbs_item_masuk(Request $request)
     {
-
-        $this->validate($request, [
-            'id_produk_tbs' => 'required|numeric',
-            'jumlah_produk' => 'required|digits_between:1,15|numeric',
-        ]);
-
+        $produk     = explode("|", $request->produk);
+        $id_produk  = $produk[0];
         $session_id = session()->getId();
 
-        $data_tbs = TbsItemMasuk::select('id_produk')
-            ->where('id_produk', $request->id_produk_tbs)
-            ->where('session_id', $session_id)
-            ->count();
+        $data_tbs = TbsItemMasuk::where('id_produk', $id_produk)->where('session_id', $session_id);
 
-        $data_produk = Barang::select('nama_barang')->where('id', $request->id_produk)->first();
-        $pesan_alert = "Produk '" . $data_produk->nama_barang . "' Sudah Ada, Silakan Pilih Produk Lain !";
+        if ($data_tbs->count() > 0) {
+            $tbsitemmasuk  = $data_tbs->first();
+            $jumlah_produk = $tbsitemmasuk->jumlah_produk + $request->jumlah_produk;
 
-        //JIKA PRODUK YG DIPILIH SUDAH ADA DI TBS
-        if ($data_tbs > 0) {
+            $data_tbs->update(['jumlah_produk' => $jumlah_produk]);
 
-            $pesan_alert =
-            '<div class="container-fluid">
-        <div class="alert-icon">
-        <i class="material-icons">warning</i>
-        </div>
-        <b>Warning : Produk "' . $data_produk->nama_barang . '" Sudah Ada, Silakan Pilih Produk Lain !</b>
-        </div>';
-
-            Session::flash("flash_notification", [
-                "level"   => "warning",
-                "message" => $pesan_alert,
-            ]);
-
-            return back();
         } else {
 
-            $pesan_alert =
-            '<div class="container-fluid">
-        <div class="alert-icon">
-        <i class="material-icons">check</i>
-        </div>
-        <b>Sukses : Berhasil Menambah Produk "' . $data_produk->nama_barang . '"</b>
-        </div>';
-
-            $tbsitemmasuk = TbsItemMasuk::create([
-                'id_produk'     => $request->id_produk_tbs,
+            TbsItemMasuk::create([
+                'id_produk'     => $id_produk,
                 'session_id'    => $session_id,
                 'jumlah_produk' => $request->jumlah_produk,
                 'warung_id'     => Auth::user()->id_warung,
             ]);
-
-            Session::flash("flash_notification", [
-                "level"   => "success",
-                "message" => $pesan_alert,
-            ]);
-            return back();
-
         }
+        return response(200);
+
     }
 
     //PROSES HAPUS TBS ITEM MASUK
--- resources/assets/js/components/item_masuk/ItemMasukCreate.vue
+++ resources/assets/js/components/item_masuk/ItemMasukCreate.vue
@@ -26,15 +26,14 @@
 
 								<div class="form-group">
 									<div class="col-md-4"><br>
-										<selectize-component v-model="item_masuk.produk" :settings="placeholder_produk" id="produk" ref='produk'> 
-											<option v-for="produks, index in produk" v-bind:value="produks.id" >{{ produks.nama_produk }}</option>
+										<selectize-component v-model="inputTbsItemMasuk.produk" :settings="placeholder_produk" id="produk" ref='produk'> 
+											<option v-for="produks, index in produk" v-bind:value="produks.produk">{{ produks.nama_produk }}</option>
 										</selectize-component>
 										<span v-if="errors.produk" id="produk_error" class="label label-danger">{{ errors.produk[0] }}</span>
 									</div> 
 								</div>
 
-								<input class="form-control" type="hidden" name="jumlah_produk" id="jumlah_produk">
-								<input class="form-control" type="hidden" name="id_produk_tbs" id="id_produk_tbs">
+								<input class="form-control" type="hidden"  v-model="inputTbsItemMasuk.jumlah_produk"  name="jumlah_produk" id="jumlah_produk">
 
 
 							</form>
@@ -87,8 +86,11 @@
 							<tbody v-if="tbs_item_masuk.length"  class="data-ada">
 								<tr v-for="tbs_item_masuk, index in tbs_item_masuk" >
 
-									<td>{{ tbs_item_masuk.nama_produk }}</td>
-									<td>{{ tbs_item_masuk.jumlah_produk }}</td>
+									<td>{{ tbs_item_masuk.kode_produk }} - {{ tbs_item_masuk.nama_produk }}</td>
+									<td>
+										<a href="#" v-bind:id="'edit-' + tbs_item_masuk.id" v-on:click="editEntry(tbs_item_masuk.id, index,tbs_item_masuk.nama_produk)">{{ tbs_item_masuk.jumlah_produk }}
+										</a>
+									</td>
 									<td> 
 										<a href="#" class="btn btn-xs btn-danger" v-bind:id="'delete-' + tbs_item_masuk.id" v-on:click="deleteEntry(tbs_item_masuk.id, index,tbs_item_masuk.nama_produk)">Delete</a>
 									</td>
@@ -104,6 +106,8 @@
 						<div align="right"><pagination :data="tbsItemMasukData" v-on:pagination-change-page="getResults" :limit="4"></pagination></div>
 
 					</div>
+					<p style="color: red; font-style: italic;">*Note : Klik Kolom Jumlah Untuk Mengubah Jumlah Produk.</p> 
+
 
 
 				</div><!-- / PANEL BODY -->
@@ -125,8 +129,9 @@ export default {
 			tbsItemMasukData : {},
 			url : window.location.origin+(window.location.pathname).replace("dashboard", "item-masuk"),
 			url_produk : window.location.origin+(window.location.pathname).replace("dashboard", "produk"),
-			item_masuk: {
-				produk : ''
+			inputTbsItemMasuk: {
+				produk : '',
+				jumlah_produk : ''
 			}, 
 			placeholder_produk: {
 				placeholder: '--PILIH PRODUK--'
@@ -148,7 +153,7 @@ export default {
         	this.getHasilPencarian();
         	this.loading = true;  
         },
-        'item_masuk.produk': function (newQuestion) {
+        'inputTbsItemMasuk.produk': function (newQuestion) {
         	this.pilihProduk();  
         },
 
@@ -219,19 +224,81 @@ export default {
     			alert("Tidak Bisa Memuat Produk");
     		});
     	},
+    	pilihProduk() {
+    		if (this.inputTbsItemMasuk.produk == '') {
+    			this.$swal({
+    				text: "Silakan Pilih Produk Telebih dahulu!",
+    			});
+    		}else{
+
+    			this.isiJumlahProduk();
+    		}
+    	},
     	isiJumlahProduk(){
-    		this.$swal({
-    			text: 'Search for a movie. e.g. "La La Land".',
-    			content: "input",
-    			button: {
-    				text: "Search!",
-    				closeModal: false,
-    			}
+    		var app = this;
+    		var produk = app.inputTbsItemMasuk.produk.split("|");
+    		var nama_produk = produk[1];
+
+    		app.$swal({
+    			title: nama_produk,
+    			content: {
+    				element: "input",
+    				attributes: {
+    					placeholder: "Jumlah Produk",
+    					type: "number",
+    				},
+    			},
+    			buttons: {
+    				cancel: true,
+    				confirm: "Submit"    				
+    			},
+
+    		}).then((value) => {
+    			if (!value) throw null;
+    			this.submitProdukItemMasuk(value);
     		});
     	},
-    	pilihProduk() {
-    		this.isiJumlahProduk();
-    	}
+    	submitProdukItemMasuk(value){
+
+    		if (value == 0) {
+
+    			this.$swal({
+    				text: "Jumlah Produk Tidak Boleh Nol!",
+    			});
+
+    		}else{
+
+    			var app = this;
+    			var produk = app.inputTbsItemMasuk.produk.split("|");
+    			var nama_produk = produk[1];
+
+    			app.inputTbsItemMasuk.jumlah_produk = value;
+    			var newinputTbsItemMasuk = app.inputTbsItemMasuk;
+
+    			axios.post(app.url+'/proses-tambah-tbs-item-masuk', newinputTbsItemMasuk)
+    			.then(function (resp) {
+    				app.getResults();
+    				app.alert("Menambahkan Produk "+nama_produk);
+    				app.inputTbsItemMasuk.jumlah_produk = ''
+    			})
+    			.catch(function (resp) {
+    				alert("Tidak dapat Menambahkan Produk");
+    			});
+    		}
+    	},
+    	editEntry(id, index,nama_produk) {
+    		
+    		var app = this;
+    		axios.delete(app.url+'/' + id)
+    		.then(function (resp) {
+    			app.getResults();
+    			app.alert("Menghapus Item Masuk "+no_faktur)
+    		})
+    		.catch(function (resp) {
+    			alert("Tidak dapat Menghapus Item Masuk");
+    		});
+    		
+    	},
     }
 }
 </script>
