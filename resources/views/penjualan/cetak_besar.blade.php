fec16f04155bf6824d3a03d5dcb48958bd9d1f2c
Riko Pernando <rikopernando96@gmail.com>
2018-01-06 14:33:19 +0700

    Perbaikan Review Sprint Demo 24
    -  Rata Kanan Angka di penjualan Index
    - Rata Kanan Angka didetail penjualan
    - Rata Kanan Angka dicetak penjualan

--- resources/views/penjualan/cetak_besar.blade.php
+++ resources/views/penjualan/cetak_besar.blade.php
@@ -40,9 +40,9 @@ th,td{
 					<div class="col-sm-8">				
 						<table>
 							<tbody>
-								<tr><td width="25%"><font class="satu">No Transaksi</font></td> <td> :&nbsp;</td> <td><font class="satu">{{$penjualan->id}}</font> </tr>
-									<tr><td  width="25%"><font class="satu">Pelanggan</font></td> <td> :&nbsp;</td> <td><font class="satu"> {{$penjualan->pelanggan}} </font></td></tr>
-									<tr><td  width="25%"><font class="satu">Alamat</font></td> <td> :&nbsp;</td> <td><font class="satu"> {{$penjualan->alamat_pelanggan}} </font></td></tr>
+								<tr><td width="25%"><font class="satu">No Transaksi</font></td> <td> :</td> <td><font class="satu">{{$penjualan->id}}</font> </tr>
+									<tr><td  width="25%"><font class="satu">Pelanggan</font></td> <td> :</td> <td><font class="satu"> {{$nama_pelanggan}} </font></td></tr>
+									<tr><td  width="25%"><font class="satu">Alamat</font></td> <td> :</td> <td><font class="satu"> {{$alamat_pelanggan}} </font></td></tr>
 								</tbody>
 							</table>
 						</div>
@@ -89,32 +89,33 @@ th,td{
 					<br>
 
 					<div class="row"><!--row1-->
-						<div class="col-sm-6">
+						<div class="col-sm-8">
 							<i><b><font class="satu">Terbilang :</font></b>{{$terbilang}}</i> <br>
 						</div><!--penutup colsm2-->
 
-						<div class="col-sm-3">
+						<div class="col-sm-2">
+							<table>
+								<tbody>
+
+									<tr><td  width="40%"><font class="satu">Kas</font></td> <td> :&nbsp;</td> <td class="text-right"><font class="satu">{{$penjualan->nama_kas}}</font> </td></tr>  
+									<tr><td  width="40%"><font class="satu">Bayar</font></td> <td> :&nbsp;</td> <td class="text-right"><font class="satu"> {{number_format($penjualan->tunai, 2, ',', '.')}}</font> </td></tr>
+									<tr><td  width="40%"><font class="satu">Kembalian</font></td> <td> :&nbsp;</td> <td class="text-right"><font class="satu">{{number_format($penjualan->kembalian, 2, ',', '.')}}</font> </td></tr> 
+
+								</tbody>
+							</table>
+
+						</div>
+
+						<div class="col-sm-2">
 
 							<table>
 								<tbody>
-									<tr><td width="50%"><font class="satu">Sub Total</font></td> <td> :&nbsp;</td> <td><font class="satu"> {{number_format($subtotal, 2, ',', '.')}} </font></tr>
-										<tr><td width="50%"><font class="satu">Diskon</font></td> <td> :&nbsp;</td> <td><font class="satu"> {{number_format($penjualan->potongan, 2, ',', '.')}}</font> </tr>
-											<tr><td  width="50%"><font class="satu">Total Akhir</font></td> <td> :&nbsp;</td> <td><font class="satu"> {{number_format($penjualan->total, 2, ',', '.')}}</font>  </td></tr>
+									<tr><td width="50%"><font class="satu">Subtotal</font></td> <td> :&nbsp;</td> <td class="text-right"><font class="satu"> {{number_format($subtotal, 2, ',', '.')}} </font></tr>
+										<tr><td width="50%"><font class="satu">Diskon</font></td> <td> :&nbsp;</td> <td class="text-right"><font class="satu"> {{number_format($penjualan->potongan, 2, ',', '.')}}</font> </tr>
+											<tr><td  width="50%"><font class="satu">Total Akhir</font></td> <td> :&nbsp;</td> <td class="text-right"><font class="satu"> {{number_format($penjualan->total, 2, ',', '.')}}</font>  </td></tr>
 										</tbody>
 									</table>
-
-								</div>
-
-								<div class="col-sm-3">
-									<table>
-										<tbody>
-
-											<tr><td  width="40%"><font class="satu">Bayar</font></td> <td> :&nbsp;</td> <td><font class="satu"> {{number_format($penjualan->tunai, 2, ',', '.')}}</font> </td></tr>
-											<tr><td  width="40%"><font class="satu">Kembali</font></td> <td> :&nbsp;</td> <td><font class="satu">{{number_format($penjualan->kembalian, 2, ',', '.')}}</font> </td></tr>
-											<tr><td  width="40%"><font class="satu">Kas</font></td> <td> :&nbsp;</td> <td><font class="satu">{{$penjualan->nama_kas}}</font> </td></tr>   
-
-										</tbody>
-									</table>
+									
 
 								</div><!--penutup colsm5-->
 							</div><!--penutup row1-->
@@ -122,7 +123,7 @@ th,td{
 							<div class="row">
 								<div class="col-sm-9">
 
-									<font class="satu"><b>Pelanggan<br><br><br> <font class="satu">{{$penjualan->pelanggan}}</font> </b></font>
+									<font class="satu"><b>Pelanggan<br><br><br> <font class="satu">{{$nama_pelanggan}}</font> </b></font>
 
 								</div> <!--/ col-sm-6-->
 
--- resources/views/penjualan/cetak_kecil.blade.php
+++ resources/views/penjualan/cetak_kecil.blade.php
@@ -20,7 +20,7 @@ p{
 	<p><b>{{$penjualan->nama_warung}}</b></p>
 	<p><b>{{$penjualan->alamat_warung}}</b></p>
 	<p>======================================</p>
-	<p>Pelanggan : {{$penjualan->pelanggan}}</p>
+	<p>Pelanggan : {{$nama_pelanggan}}</p>
 	<p>-------------------------------------------------------------</p>
 	<p>No. Transaksi : #{{$penjualan->id}}</p>
 
@@ -29,33 +29,33 @@ p{
 
 	<table>
 		<tbody>
-			@foreach ($detail_penjualan as $detail_penjualans)			
-			<tr><td width:"50%"> {{title_case($detail_penjualans->produk->nama_barang)}} </td><td style="padding:3px"> {{number_format($detail_penjualans->harga_produk, 2, ',', '.')}}</td><td style="padding:3px">{{number_format($detail_penjualans->jumlah_produk, 2, ',', '.')}}</td><td style="padding:3px">{{number_format($detail_penjualans->subtotal, 2, ',', '.')}}</td></tr>
-			@endforeach
 
+			@foreach ($detail_penjualan as $detail_penjualans)			
+			<tr><td style="padding: 3px;"> {{title_case($detail_penjualans->produk->nama_barang)}} </td><td style="padding:3px" align="right"> {{number_format($detail_penjualans->harga_produk, 2, ',', '.')}}</td><td  align="right" style="padding:3px">{{number_format($detail_penjualans->jumlah_produk, 2, ',', '.')}}</td><td  align="right" style="padding:3px">{{number_format($detail_penjualans->harga_produk * $detail_penjualans->jumlah_produk, 2, ',', '.')}}</td></tr>
+			@endforeach
+			<tr><td style="padding: 3px;"> Subtotal </td><td style="padding:3px" align="right">:</td><td  align="right" style="padding:3px"></td><td  align="right" style="padding:3px">{{number_format($subtotal, 2, ',', '.')}}</td></tr>
+			<tr><td style="padding: 3px;"> Diskon </td><td style="padding:3px" align="right">:</td><td  align="right" style="padding:3px"></td><td  align="right" style="padding:3px">{{number_format($potongan, 2, ',', '.')}}</td></tr>
+			<tr><td style="padding: 3px;"> Total </td><td style="padding:3px" align="right">:</td><td  align="right" style="padding:3px"></td><td  align="right" style="padding:3px">{{number_format($penjualan->total, 2, ',', '.')}}</td></tr>
+			<tr><td style="padding: 3px;"> Tunai </td><td style="padding:3px" align="right">:</td><td  align="right" style="padding:3px"></td><td  align="right" style="padding:3px">{{number_format($penjualan->tunai, 2, ',', '.')}}</td></tr>
+			<tr><td style="padding: 3px;"> Kembalian </td><td style="padding:3px" align="right">:</td><td  align="right" style="padding:3px"></td><td  align="right" style="padding:3px">{{number_format($penjualan->kembalian, 2, ',', '.')}}</td></tr>
 		</tbody>
 	</table>
-
-	<p>-------------------------------------------------------------</p>
-	<p>Subtotal : {{number_format($subtotal, 2, ',', '.')}}</p>
-	<p>Diskon : {{number_format($penjualan->potongan, 2, ',', '.')}}</p>
-	<p>Total : {{number_format($penjualan->total, 2, ',', '.')}}</p>
-	<p>Tunai : {{number_format($penjualan->tunai, 2, ',', '.')}}</p>
-	<p>Kembalian : {{number_format($penjualan->kembalian, 2, ',', '.')}}</p>
+	
 	<p>======================================</p>
 	<p>{{$penjualan->waktu_jual}}</p>
 
 	<p>Terima Kasih </p>
-	<p>No. Telp : {{$penjualan->no_telp_warung}}</p>
+	<p>No. Telp : {{$penjualan->no_telp_warung}}</p>	
+	<p><i>Support by andaglos.id</i></p><br><br>
 
 </body>
 <!--   Core JS Files   -->
 <script src="{{ asset('js/app.js?v=1.51')}}" type="text/javascript"></script>
 
 <script>
-$(document).ready(function(){
-	window.print();
-});
+	$(document).ready(function(){
+		window.print();
+	});
 </script>
 @yield('scripts')
 </html>
