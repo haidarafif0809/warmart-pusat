ab4a8dbf9af57794e0e39283fc32b823ca7c10f3
Riko Pernando <rikopernando96@gmail.com>
2018-01-09 14:29:27 +0700

    pasang if aplikasi di halaman selesaikan pesanan warung
    pasang if aplikasi di halaman selesaikan pesanan warung

--- resources/views/layouts/selesaikan_pemesanan.blade.php
+++ resources/views/layouts/selesaikan_pemesanan.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.app_pelanggan')
 @section('content')
+<style type="text/css">
+.page-header.header-small {
+  height: 35vh;
+  min-height: 35vh;
+}
+.ecommerce-page .page-header .container {
+  padding-top: 10vh;
+}
+h4 {
+  @if(Agent::isMobile())
+  font-size: 1.2em;
+  line-height: 1.4em;
+  margin: 20px 0 10px;
+  @endif
+}
+.panel .panel-heading {
+  background-color: transparent;
+  border-bottom: 2px solid #ddd;
+  padding: 5px 0px 5px 0px;
+}
+</style>
 
 <div class="page-header header-filter header-small" data-parallax="true" style="background-image: url('image/background2.jpg');">
 
@@ -7,8 +28,17 @@
     <div class="row">
       <div class="col-md-8 col-md-offset-2">
         <div class="brand">
-          <h1 class="title">PASAR MUSLIM INDONESIA</h1>
-          <h4 class="title"> Segala Kemudahan Untuk Umat Muslim Berbelanja.</h4>
+          <?php
+          $setting_aplikasi = \App\SettingAplikasi::select('tipe_aplikasi')->first();
+          ?>
+          @if($setting_aplikasi->tipe_aplikasi == 0)
+          <h1 class="title"> PASAR MUSLIM INDONESIA </h1>
+          <h4 class="title"> Segala Kemudahan Untuk Umat Muslim Berbelanja. </h4>
+          @else
+          <h1 class="title"> TOKO ONLINE DAN POS </h1>
+          @endif
+
+
         </div>
       </div>
     </div>
@@ -74,228 +104,228 @@
         </div>
 
         <div class="col-md-5">
-         @if ($agent->isMobile()) <!--JIKA DAKSES VIA HP/TAB-->
+          @if ($agent->isMobile()) <!--JIKA DAKSES VIA HP/TAB-->
 
-         <div class="card" style="margin-top: 1px; margin-bottom: 1px; margin-left: 1px;">
+          <div class="card" style="margin-top: 1px; margin-bottom: 1px; margin-left: 1px;">
 
-           @foreach($keranjang_belanjaan as $keranjang_belanjaans)
-           <div class="row" style="margin-bottom: 1px;">
-            <div class="col-md-12">
+            @foreach($keranjang_belanjaan as $keranjang_belanjaans)
+            <div class="row" style="margin-bottom: 1px;">
+              <div class="col-md-12">
 
-              <div class="row">
-                <div class="col-xs-4">
-                  <div class="img-container" style="margin-bottom:5px;margin-top: 5px; margin-left: 5px; margin-right: 5px;">
-                   @if($keranjang_belanjaans->produk->foto != NULL)
-                   <img src="foto_produk/{{$keranjang_belanjaans->produk->foto}}">
-                   @else
-                   <img src="image/foto_default.png">
-                   @endif
-                 </div>
+                <div class="row">
+                  <div class="col-xs-4">
+                    <div class="img-container" style="margin-bottom:5px;margin-top: 5px; margin-left: 5px; margin-right: 5px;">
+                      @if($keranjang_belanjaans->produk->foto != NULL)
+                      <img src="foto_produk/{{$keranjang_belanjaans->produk->foto}}">
+                      @else
+                      <img src="image/foto_default.png">
+                      @endif
+                    </div>
 
-               </div>
+                  </div>
 
-               <div class="col-xs-8">
-                <p style="margin-bottom:1px;margin-top: 1px;"><a href="#"><b>{{$keranjang_belanjaans->NamaProdukMobile}}</b></a></p>
-                <div class="responsive">
-                 <table>
-                  <tbody>
-                    <tr style="font-weight: bold">
-                      <td width="25%"> {{number_format($keranjang_belanjaans->produk->harga_jual,0,',','.') }} </td>
-                      <td>&nbsp; x </td>
-                      <td> {{$keranjang_belanjaans->jumlah_produk }} </td>
-                      <td> {{number_format($keranjang_belanjaans->produk->harga_jual * $keranjang_belanjaans->jumlah_produk,0,',','.') }} </td>
-                    </tr>
-                  </tbody>
-                </table>
+                  <div class="col-xs-8">
+                    <p style="margin-bottom:1px;margin-top: 1px;"><a href="#"><b>{{$keranjang_belanjaans->NamaProdukMobile}}</b></a></p>
+                    <div class="responsive">
+                      <table>
+                        <tbody>
+                          <tr style="font-weight: bold">
+                            <td width="25%"> {{number_format($keranjang_belanjaans->produk->harga_jual,0,',','.') }} </td>
+                            <td>&nbsp; x </td>
+                            <td> {{$keranjang_belanjaans->jumlah_produk }} </td>
+                            <td> {{number_format($keranjang_belanjaans->produk->harga_jual * $keranjang_belanjaans->jumlah_produk,0,',','.') }} </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                    <p style="margin-bottom:1px;margin-top: 1px;"><small>{{$keranjang_belanjaans->produk->warung->name}}</small></p>
+
+                  </div>
+
+                </div>
+
               </div>
-              <p style="margin-bottom:1px;margin-top: 1px;"><small>{{$keranjang_belanjaans->produk->warung->name}}</small></p>
+            </div><hr style="margin-bottom: 1px; margin-top: 1px;">
 
-            </div>
+            @endforeach
 
           </div>
 
+          {{$pagination}}
+          <div class="card" style="margin-bottom: 1px; margin-top: 1px;">
+            <div class="row">
+              <div class="col-xs-6">
+                <p style="margin-left: 5px; margin-top: 5px; margin-bottom: 5px;"><b> Total Produk</b></p>
+              </div>
+              <div class="col-xs-6">
+                <p style="margin-left: 5px; margin-top: 5px; margin-bottom: 5px;"><b> {{ $jumlah_produk->total_produk }}</b></p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-xs-4">
+                <p style="margin-left: 5px; margin-top: 5px; margin-bottom: 5px;"><b>Total</b></p>
+              </div>
+              <div class="col-xs-2">
+
+              </div>
+              <div class="col-xs-6">
+                <p style="margin-left: 5px; margin-top: 5px; margin-bottom: 5px;"><b>Rp. {{ number_format($subtotal,0,',','.') }}</b></p>
+              </div>
+            </div>
+          </div>
+
+          <center>{!! Form::button('Selesai Pesanan <i class="material-icons">keyboard_arrow_right</i> ', ['id'=>'SelesaikanPesanan','class'=>'btn btn-round', 'type'=>'submit','style'=>'background-color: #01573e']) !!}</center>
+
+          @else
+
+          <div class="card" style="margin-bottom: 5px; margin-top: 5px;">
+            <div class="card-header" style="padding-bottom: 1px;">
+              <h6 class="card-title" style="color: black; padding-left: 10px; margin-bottom: 1px;">Rincian Pesanan</h6> <hr>
+            </div>
+
+            <div class="card-content" style="padding-top: 1px; padding-bottom: 1px;">
+              <div class="row">
+                <div class="col-md-4"><h5><b>Produk</b></h5> </div>
+                <div class="col-md-2"><h5><b>Jumlah</b></h5> </div>
+                <div class="col-md-3"><h5><b>Harga</b></h5> </div>
+                <div class="col-md-3"><h5><b>Subtotal</b></h5> </div>
+              </div>
+
+              @foreach($keranjang_belanjaan as $keranjang_belanjaans)
+              <div class="row">
+
+                <div class="col-md-4">
+                  <li><b> <a href="{{ url('detail-produk/'.$keranjang_belanjaans->id_produk.'') }}">{{ $keranjang_belanjaans->NamaProduk }}</a></b></li>
+                </div>
+
+                <div class="col-md-2">
+                  <b align="right">{{ $keranjang_belanjaans->jumlah_produk }}</b>
+                </div>
+
+                <div class="col-md-3">
+                  <b align="right">
+                    {{ number_format($keranjang_belanjaans->produk->harga_jual,0,',','.') }}</b>
+                  </div>
+
+                  <div class="col-md-3">
+
+                    <div class="row">
+
+                      <div class="col-md-8">
+                        <b>{{ number_format($keranjang_belanjaans->produk->harga_jual * $keranjang_belanjaans->jumlah_produk,0,',','.') }}</b>
+                      </div>
+
+                      <div class="col-md-4">
+                        <a href="#" id="btnHapusProduk" data-nama="{{title_case($keranjang_belanjaans->produk->nama_barang)}}" data-id="{{$keranjang_belanjaans->id_keranjang_belanja}}" type="button"><i class="material-icons">close</i></a>
+                      </div>
+
+                    </div>
+
+                  </div>
+
+                </div>
+                @endforeach
+                {{$pagination}}
+
+              </div>
+              <hr style="margin-top: 1px;">
+              <div class="card-content" style="margin-top: 1px;">
+
+                <div class="row">
+                  <div class="col-md-4"><b>Total Produk</b></div>
+                  <div class="col-md-4"><b>:</b></div>
+                  <div class="col-md-4"><b>{{ $jumlah_produk->total_produk }}</b></div>
+                </div>
+
+                <div class="row">
+                  <div class="col-md-4"><h5><b>Total </b></h5></div>
+                  <div class="col-md-4"><h5><b>:</h5></div>
+                    <div class="col-md-4"><h5 class="text-danger"><b>RP {{ number_format($subtotal,0,',','.') }}</b></h5></div>
+                  </div>
+                </div>
+
+              </div>
+
+              {!! Form::button('Selesai Pesanan <i class="material-icons">keyboard_arrow_right</i> ', ['id'=>'SelesaikanPesanan','class'=>'btn btn-round pull-right', 'type'=>'submit','style'=>'background-color: #01573e']) !!}
+              @endif
+
+              {!! Form::close() !!}
+            </div>
+            @endif
+          </div>
         </div>
-      </div><hr style="margin-bottom: 1px; margin-top: 1px;">
-
-      @endforeach
-
-    </div>
-
-    {{$pagination}}
-    <div class="card" style="margin-bottom: 1px; margin-top: 1px;">
-      <div class="row">
-        <div class="col-xs-6">
-         <p style="margin-left: 5px; margin-top: 5px; margin-bottom: 5px;"><b> Total Produk</b></p>
-       </div>
-       <div class="col-xs-6">
-        <p style="margin-left: 5px; margin-top: 5px; margin-bottom: 5px;"><b> {{ $jumlah_produk->total_produk }}</b></p>
       </div>
     </div>
-    <div class="row">
-      <div class="col-xs-4">
-       <p style="margin-left: 5px; margin-top: 5px; margin-bottom: 5px;"><b>Total</b></p>
-     </div>
-     <div class="col-xs-2">
-
-     </div>
-     <div class="col-xs-6">
-      <p style="margin-left: 5px; margin-top: 5px; margin-bottom: 5px;"><b>Rp. {{ number_format($subtotal,0,',','.') }}</b></p>
-    </div>
-  </div>
-</div>
-
-<center>{!! Form::button('Selesai Pesanan <i class="material-icons">keyboard_arrow_right</i> ', ['id'=>'SelesaikanPesanan','class'=>'btn btn-round', 'type'=>'submit','style'=>'background-color: #01573e']) !!}</center>
-
-@else
-
-<div class="card" style="margin-bottom: 5px; margin-top: 5px;">
-  <div class="card-header" style="padding-bottom: 1px;">
-    <h6 class="card-title" style="color: black; padding-left: 10px; margin-bottom: 1px;">Rincian Pesanan</h6> <hr>
-  </div>
-
-  <div class="card-content" style="padding-top: 1px; padding-bottom: 1px;">
-    <div class="row">
-      <div class="col-md-4"><h5><b>Produk</b></h5> </div>
-      <div class="col-md-2"><h5><b>Jumlah</b></h5> </div>
-      <div class="col-md-3"><h5><b>Harga</b></h5> </div>
-      <div class="col-md-3"><h5><b>Subtotal</b></h5> </div>
-    </div>
-
-    @foreach($keranjang_belanjaan as $keranjang_belanjaans)
-    <div class="row">
-
-      <div class="col-md-4">
-       <li><b> <a href="{{ url('detail-produk/'.$keranjang_belanjaans->id_produk.'') }}">{{ $keranjang_belanjaans->NamaProduk }}</a></b></li>
-     </div>
-
-     <div class="col-md-2">
-      <b align="right">{{ $keranjang_belanjaans->jumlah_produk }}</b>
-    </div>
-
-    <div class="col-md-3">
-      <b align="right">
-       {{ number_format($keranjang_belanjaans->produk->harga_jual,0,',','.') }}</b>
-     </div>
-
-     <div class="col-md-3">
-
-       <div class="row">
-
-        <div class="col-md-8">
-          <b>{{ number_format($keranjang_belanjaans->produk->harga_jual * $keranjang_belanjaans->jumlah_produk,0,',','.') }}</b>
-        </div>
-
-        <div class="col-md-4">
-          <a href="#" id="btnHapusProduk" data-nama="{{title_case($keranjang_belanjaans->produk->nama_barang)}}" data-id="{{$keranjang_belanjaans->id_keranjang_belanja}}" type="button"><i class="material-icons">close</i></a>
-        </div>
-
-      </div>
-
-    </div>
-
-  </div>
-  @endforeach
-  {{$pagination}}
-
-</div>
-<hr style="margin-top: 1px;">
-<div class="card-content" style="margin-top: 1px;">
-
-  <div class="row">
-    <div class="col-md-4"><b>Total Produk</b></div>
-    <div class="col-md-4"><b>:</b></div>
-    <div class="col-md-4"><b>{{ $jumlah_produk->total_produk }}</b></div>
-  </div>
-
-  <div class="row">
-    <div class="col-md-4"><h5><b>Total </b></h5></div>
-    <div class="col-md-4"><h5><b>:</h5></div>
-    <div class="col-md-4"><h5 class="text-danger"><b>RP {{ number_format($subtotal,0,',','.') }}</b></h5></div>
-  </div>
-</div>
-
-</div>
-
-{!! Form::button('Selesai Pesanan <i class="material-icons">keyboard_arrow_right</i> ', ['id'=>'SelesaikanPesanan','class'=>'btn btn-round pull-right', 'type'=>'submit','style'=>'background-color: #01573e']) !!}
-@endif
-
-{!! Form::close() !!}
-</div>
-@endif
-</div>
-</div>
-</div>
-</div>
-</div> <!-- end-main-raised -->
-@endsection
+  </div> <!-- end-main-raised -->
+  @endsection
 
 
-@section('scripts')
-<script type="text/javascript">
+  @section('scripts')
+  <script type="text/javascript">
 
-  $(document).on('click', '#SelesaikanPesanan', function () {
+    $(document).on('click', '#SelesaikanPesanan', function () {
 
-    $("#formSelesaikanPesanan").submit(function(){
-      return false;
+      $("#formSelesaikanPesanan").submit(function(){
+        return false;
+      });
+
+      swal({
+        html: "Anda Yakin Ingin Menyelesaikan Pesanan Ini ?",
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Ya',
+        cancelButtonText: 'Batal'
+      }).then((result) => {
+        if (result.value) {
+
+          document.getElementById("formSelesaikanPesanan").submit();
+
+          swal({
+            html :  "Berhasil Menyelesaikan Pesanan."+"<p>Terima Kasih Telah Berbelanja Di Warmart , Silahkan Tunggu Konfirmasi Dari Warung<p>",
+            showConfirmButton :  false,
+            type: "success",
+            timer: 10000,
+            onOpen: () => {
+              swal.showLoading()
+            }
+          });
+
+        }
+      })
+
+
     });
 
-    swal({
-      html: "Anda Yakin Ingin Menyelesaikan Pesanan Ini ?",
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Ya',
-      cancelButtonText: 'Batal'
-    }).then((result) => {
-      if (result.value) {
+    $(document).on('click', '#btnHapusProduk', function () {
+      var id = $(this).attr("data-id");
+      var nama = $(this).attr("data-nama");
 
-        document.getElementById("formSelesaikanPesanan").submit();
+      swal({
+        html: "Anda Yakin Ingin Menghapus Produk <b>"+nama+"</b> Dari Keranjang Belanja ?",
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Ya',
+        cancelButtonText: 'Batal'
+      }).then((result) => {
+        if (result.value) {
+          var url_hapus_produk_keranjang_belanja = window.location.origin + (window.location.pathname).replace("keranjang-belanja", "keranjang-belanja/hapus-produk-keranjang-belanja/"+id);
+          window.location.href=url_hapus_produk_keranjang_belanja;
+          swal({
+            html :  "Produk <b>"+nama+"</b> Berhasil Dihapus Dari Keranjang Belanjaan",
+            showConfirmButton :  false,
+            type: "success",
+            timer: 10000,
+            onOpen: () => {
+              swal.showLoading()
+            }
+          });
 
-        swal({
-          html :  "Berhasil Menyelesaikan Pesanan."+"<p>Terima Kasih Telah Berbelanja Di Warmart , Silahkan Tunggu Konfirmasi Dari Warung<p>",
-          showConfirmButton :  false,
-          type: "success",
-          timer: 10000,
-          onOpen: () => {
-            swal.showLoading()
-          }
-        });
+        }
+      })
 
-      }
-    })
+    });
 
-
-  });
-
-  $(document).on('click', '#btnHapusProduk', function () {
-    var id = $(this).attr("data-id");
-    var nama = $(this).attr("data-nama");
-
-    swal({
-      html: "Anda Yakin Ingin Menghapus Produk <b>"+nama+"</b> Dari Keranjang Belanja ?",
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Ya',
-      cancelButtonText: 'Batal'
-    }).then((result) => {
-      if (result.value) {
-        var url_hapus_produk_keranjang_belanja = window.location.origin + (window.location.pathname).replace("keranjang-belanja", "keranjang-belanja/hapus-produk-keranjang-belanja/"+id);
-        window.location.href=url_hapus_produk_keranjang_belanja;
-        swal({
-          html :  "Produk <b>"+nama+"</b> Berhasil Dihapus Dari Keranjang Belanjaan",
-          showConfirmButton :  false,
-          type: "success",
-          timer: 10000,
-          onOpen: () => {
-            swal.showLoading()
-          }
-        });
-
-      }
-    })
-
-  });
-
-</script>
-@endsection
+  </script>
+  @endsection
