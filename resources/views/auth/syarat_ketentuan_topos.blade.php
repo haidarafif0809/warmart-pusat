242b3e54da32b5715e9b41cecd05903607fd60cc
fahrizalrahman <fahrizal95rhm@gmail.com>
2018-01-10 09:56:24 +0700

    Perbaikan logo topos di syarat ketentuan

--- resources/views/auth/syarat_ketentuan_topos.blade.php
+++ resources/views/auth/syarat_ketentuan_topos.blade.php
@@ -43,7 +43,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="" ><p style="color:white;">To-Pos.id</p></a>
+                <a class="navbar-brand" href="{{ url('/')}}"><img class="navbar-brand" src="{{asset('/assets/img/examples/topos_logo.png')}}"/></a>
             </div>
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav navbar-right"> 
