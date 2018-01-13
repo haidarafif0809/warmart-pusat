e65afda805d98c209eee12716dee346dcc1a7793
RindangRamadhan <rindangramadhan10@gmail.com>
2018-01-13 15:23:27 +0700

    Form Kas Keluar
    Tambah tombol pintsan untuk membuat master Kas dan Kategori Transaksi

--- resources/assets/js/components/kas_keluar/KasKeluarCreate.vue
+++ resources/assets/js/components/kas_keluar/KasKeluarCreate.vue
@@ -1,3 +1,10 @@
+<style scoped>
+  .btn-icon{
+    border-radius: 1px solid;
+    padding: 10px 10px;
+  }
+</style>
+
 <template>
 	
 	<div class="row" >
@@ -19,20 +26,32 @@
 		               	<form v-on:submit.prevent="saveForm()" class="form-horizontal"> 
 		                    <div class="form-group">
 		                        <label for="kas" class="col-md-2 control-label">Kas</label>
-		                        <div class="col-md-4">
+		                        <div class="col-md-4 col-xs-10">
 		                            <selectize-component v-model="kasKeluar.kas" :settings="placeholderKas" id="kas"  name="kas"> 
 	                                    <option v-for="data_kas, index in kas" v-bind:value="data_kas.id">{{ data_kas.nama_kas }}</option>
 	                                </selectize-component>
 		                        </div>
+
+	                            <div class="col-md-1 col-xs-1" style="padding-left:0px">
+	                              <div class="row" style="margin-top:-10px">
+	                                <button class="btn btn-primary btn-icon waves-effect waves-light" @click="tambahKasBaru()" type="button"> <i class="material-icons" style="font-size:14px">add</i> </button>
+	                              </div>
+	                            </div>  
 		                    </div>
 
 		                    <div class="form-group">
 		                        <label for="kategori" class="col-md-2 control-label">Kategori</label>
-		                        <div class="col-md-4">
+		                        <div class="col-md-4 col-xs-10">
 		                             <selectize-component v-model="kasKeluar.kategori" :settings="placeholderKategori" id="kategori_transaksi" name="kategori_transaksi">
 	                                    <option v-for="data_kategori, index in kategori" v-bind:value="data_kategori.id">{{ data_kategori.nama_kategori_transaksi }}</option>
 	                                </selectize-component>
 		                        </div>
+		                        
+	                            <div class="col-md-1 col-xs-1" style="padding-left:0px">
+	                              <div class="row" style="margin-top:-10px">
+	                                <button class="btn btn-primary btn-icon waves-effect waves-light" @click="kategoriBaru()" type="button"> <i class="material-icons" style="font-size:14px">add</i> </button>
+	                              </div>
+	                            </div>  
 		                    </div>
 
 		                    <div class="form-group">
@@ -74,11 +93,22 @@ export default {
             kas : [],
             kategori :[],
             url : window.location.origin+(window.location.pathname).replace("dashboard", "kas-keluar"),
+            urlKategoriTransaksi : window.location.origin+(window.location.pathname).replace("dashboard", "kategori-transaksi"),
+            urlKas : window.location.origin+(window.location.pathname).replace("dashboard", "kas"),
             kasKeluar: {
                 kas: '',
                 kategori: '',
                 jumlah: '',
                 keterangan: ''
+            },
+            kategoriTransaksi: {
+                nama_kategori_transaksi: '',
+            },
+            kasBaru: {
+              kode_kas : 'K001',
+              nama_kas : '',
+              status_kas : 1,
+              default_kas : 0
             },
             message : '',
             placeholderKas: {
@@ -147,6 +177,14 @@ export default {
               icon: "warning",
           });
       	},
+        alertBerhasil(pesan) {
+          this.$swal({
+              title: "Sukses!",
+              text: pesan,
+              icon: "success",
+              timer: 1500,
+          });
+        },
       	dataKas() {
 	      	var app = this;
 	      	axios.get(app.url+'/pilih-kas').then(function (resp) {
@@ -165,6 +203,100 @@ export default {
 	        	alert("Tidak Bisa Memuat Kategori");
 	        });
 	    },
+      kategoriBaru(){
+          var app = this;
+          app.$swal({
+                title: "Kategori Transaksi",
+                content: {
+                    element: "input",
+                    attributes: {
+                        placeholder: "Nama Kategori Transaksi",
+                        type: "text",
+                    },
+                },
+                closeOnEsc: true,
+                buttons: {
+                    confirm: "OK"                   
+                }
+          }).then((value) => {
+              this.tambahKategoriTransaksiBaru(value);
+        });
+      },
+      tambahKategoriTransaksiBaru(value){
+        if (value == "") {
+          this.$swal({
+              text: "Nama Kategori Transaksi Tidak Boleh Kosong!",
+          });
+        }else{
+          var app = this;
+          app.kategoriTransaksi.nama_kategori_transaksi = value;
+          var newKategoriTransaksi = app.kategoriTransaksi;
+
+          axios.post(app.urlKategoriTransaksi, newKategoriTransaksi).then(function (resp) {
+
+              app.dataKategori();
+              app.message = 'Berhasil Menambah Kategori Transaksi '+ app.kategoriTransaksi.nama_kategori_transaksi;
+              app.alertBerhasil(app.message);
+              app.$router.replace('/create-kas-keluar');
+              timer: 2000
+              console.log(resp);
+          })
+          .catch(function (resp) {
+              console.log(resp);
+              app.kategoriTransaksi = ''
+              app.$router.replace('/create-kas-keluar');
+          });
+        }
+      },
+      tambahKasBaru(){
+          var app = this;
+          swal({
+            title: "Kas",
+            html:
+              '<input class="form-control" autocomplete="off" placeholder="Kode Kas" type="text" name="kode_kas" id="kode_kas" autofocus="" required="">' +
+              '<input class="form-control" autocomplete="off" placeholder="Nama Kas" type="text" name="nama_kas" id="nama_kas" required="">',
+            allowEnterKey : false,
+            showCloseButton: true, 
+            showCancelButton: true,                        
+            focusConfirm: false, 
+            confirmButtonText:'OK', 
+            confirmButtonAriaLabel: 'Thumbs up, great!', 
+            cancelButtonText:'Batal', 
+            closeOnConfirm: false, 
+            cancelButtonAriaLabel: 'Thumbs down', 
+            preConfirm: function () {
+              return new Promise(function (resolve) { 
+                resolve([ 
+                  $('#kode_kas').val(), 
+                  $('#nama_kas').val(),
+                ]) 
+              }) 
+            }
+        }).then((result) => {
+              app.prosesTambahKasBaru(result);
+        });
+      },
+      prosesTambahKasBaru(result){
+        var app = this;
+        app.kasBaru.kode_kas = result[0];
+        app.kasBaru.nama_kas = result[1];
+        var newKas = app.kasBaru;
+
+        axios.post(app.urlKas, newKas)
+        .then(function (resp) {
+
+              app.dataKas();
+              app.message = 'Berhasil Menambah Kas '+ app.kasBaru.nama_kas;
+              app.alertBerhasil(app.message);
+              app.$router.replace('/create-kas-keluar');
+              timer: 2000
+        })
+        .catch(function (resp) {
+          console.log(resp);
+          app.success = false;
+          app.errors = resp.response.data.errors;
+        });
+      }
   	}
 }
 </script>
