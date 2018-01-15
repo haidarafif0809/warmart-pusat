9daead41b700354d8ef883e55998fe49de564b6f
fahrizalrahman <fahrizal95rhm@gmail.com>
2018-01-15 10:41:09 +0700

    TOMBOL JALAN PINTAS KAS (KAS MUTASI EDIT)

--- resources/assets/js/components/kas_mutasi/KasMutasiEdit.vue
+++ resources/assets/js/components/kas_mutasi/KasMutasiEdit.vue
@@ -1,3 +1,9 @@
+<style scoped>
+  .btn-icon{
+    border-radius: 1px solid;
+    padding: 10px 10px;
+  }
+</style>
 <template>
     <div class="row" >
         <div class="col-md-12">
@@ -6,6 +12,67 @@
                 <li><router-link :to="{name: 'indexKasMutasi'}">Kas Mutasi</router-link></li>
                 <li class="active">Edit Kas Mutasi</li>
             </ul>
+
+                         <div class="modal" id="modal_tambah_kas" role="dialog" data-backdrop=""> 
+                <div class="modal-dialog"> 
+                    <!-- Modal content--> 
+                    <div class="modal-content"> 
+                        <div class="modal-header"> 
+                            <button type="button" class="close"  v-on:click="closeModalX()" v-shortkey.push="['esc']" @shortkey="closeModalX()"> &times;</button> 
+                            <h4 class="modal-title"> 
+                                <div class="alert-icon"> 
+                                    <b>Silahkan Isi Kas!</b> 
+                                </div> 
+                            </h4> 
+                        </div> 
+                          <div class="modal-body">
+                        <form v-on:submit.prevent="saveFormKas()" class="form-horizontal"> 
+                          <div class="form-group">
+                            <label for="kode_kas" class="col-md-3 control-label">Kode Kas</label>
+                            <div class="col-md-9">
+                              <input class="form-control" autocomplete="off" placeholder="Kode Kas" v-model="tambahKas.kode_kas" type="text" name="kode_kas" id="kode_kas"  autofocus="">
+                              <span v-if="errors.kode_kas" id="kode_kas_error" class="label label-danger">{{ errors.kode_kas[0] }}</span>
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label for="nama_kas" class="col-md-3 control-label">Nama Kas</label>
+                            <div class="col-md-9">
+                              <input class="form-control" autocomplete="off" placeholder="Nama Kas" v-model="tambahKas.nama_kas" type="text" name="nama_kas" id="nama_kas"  >
+                              <span v-if="errors.nama_kas" id="nama_kas_error" class="label label-danger">{{ errors.nama_kas[0] }}</span>
+                            </div>
+                          </div>
+                          <div class="form-group">
+                          <label for="nama_kas" class="col-md-3 control-label">Tampil Transaksi</label>
+                               <div class="togglebutton col-md-9">
+                                     <label>
+                                         <b>No</b>  <input type="checkbox" v-model="tambahKas.status_kas" value="1" name="status_kas" id="status_kas"><b>Yes</b>
+                                         </label>
+                                          </div>
+                                      </div>
+                                      <div class="form-group">
+                                          <label for="nama_kas" class="col-md-3 control-label">Default Kas</label>
+                                              <div class="togglebutton col-md-9">
+                                              <label>
+                                                  <b>No</b>  <input type="checkbox" @change="defaultKas()" v-model="tambahKas.default_kas" value="1" name="default_kas" id="default_kas"><b>Yes</b>
+                                              </label>
+                                          </div>
+                                  </div>
+                              <div class="form-group">
+                              <div class="col-md-9 col-md-offset-3">
+                                <p style="color: red; font-style: italic;">*Note : Hanya 1 Kas yang dijadikan default.</p>
+                              <button class="btn btn-primary" id="btnSimpanKas" type="submit"><i class="material-icons">send</i> Submit</button>
+                            </div>
+                          </div>
+                        </form>
+                          </div>
+                    <div class="modal-footer">  
+                   </div> 
+           </div>       
+       </div> 
+   </div> 
+   <!-- / MODAL TOMBOL SELESAI --> 
+
+
             <div class="card">
                <div class="card-header card-header-icon" data-background-color="purple">
                    <i class="material-icons">money_off</i>
@@ -16,12 +83,17 @@
                     <form v-on:submit.prevent="saveForm()" class="form-horizontal"> 
                             <div class="form-group">
                                 <label for="dari_kas" class="col-md-2 control-label">Dari Kas</label>
-                                <div class="col-md-4">
+                                <div class="col-md-4 col-xs-10">
                                     <selectize-component v-model="kasMutasi.dari_kas" :settings="placeholderKas" id="dari_kas"  name="dari_kas"> 
                                         <option v-for="data_kas, index in kas" v-bind:value="data_kas.id">{{ data_kas.nama_kas }}</option>
                                     </selectize-component>
                                     <span v-if="errors.dari_kas" id="dari_kas_error" class="label label-danger">{{ errors.dari_kas[0] }}</span>
                                 </div>
+                              <div class="col-md-1 col-xs-1" style="padding-left:0px;">
+                              <div class="row" style="margin-top:-10px">
+                                <button class="btn btn-primary btn-icon waves-effect waves-light" @click="tambahModalKas()" type="button"> <i class="material-icons" style="font-size:14px">add</i> </button>
+                              </div>
+                             </div>
                             </div>
 
                             <div class="form-group">
@@ -95,8 +167,15 @@ export default {
                 placeholder: '--PILIH KAS--'
             },
             url : window.location.origin+(window.location.pathname).replace("dashboard", "kas-mutasi"),
+            urlKas : window.location.origin+(window.location.pathname).replace("dashboard", "kas"),
             errors: [],
             kas : [],
+            tambahKas: {
+            kode_kas : '',
+            nama_kas : '',
+            status_kas : 0,
+            default_kas : 0
+            },
             separator: {
               decimal: ',',
               thousands: '.',
@@ -172,7 +251,56 @@ export default {
             app.message = 'Berhasil Menambah Kas Mutasi';
             app.alert(app.message);
             app.$router.replace('/kas-mutasi');
-        }
+        },
+        closeModalX(){
+        $("#modal_tambah_kas").hide();  
+        },
+        tambahModalKas(){
+           $("#modal_tambah_kas").show();
+           this.$refs.kode_kas.$el.focus(); 
+          },
+        saveFormKas() {
+              var app = this;
+              var newkas = app.tambahKas;
+              axios.post(app.urlKas, newkas)
+              .then(function (resp) {
+                app.message = 'Menambah Kategori Transaksi '+ app.tambahKas.nama_kas;
+                app.alert(app.message);
+                app.tambahKas.kode_kas = ''
+                app.tambahKas.nama_kas = ''
+                app.tambahKas.status_kas = 0
+                app.tambahKas.default_kas = 0
+                app.errors = '';
+                 app.dataKas();
+                $("#modal_tambah_kas").hide();
+      })
+      .catch(function (resp) {
+        app.success = false;
+        app.errors = resp.response.data.errors;
+      });
+    },
+    defaultKas() {
+         var app = this;
+          var toogle = app.tambahKas.default_kas;
+
+         if (toogle == true) {
+          app = this;
+      app.$swal({
+      title: "Konfirmasi",
+      text: "Apakah Anda Yakin Ingin Mengubah Kas Utama ?",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+      })
+      .then((confirm) => {
+        if (confirm) {
+        toogle.prop('checked', true);
+        } else {
+        toogle.prop('checked', false);
+      }
+      });
+         }  
+      }
     }
 }
 </script>
