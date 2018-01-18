de511b1bff44392e7ca9b677115ea5234645c20f
Riko Pernando <rikopernando96@gmail.com>
2018-01-18 13:29:36 +0700

    Run production & update versi app.js
    Run production & update versi app.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,9 +43,9 @@
 </head>
 <style type="text/css">
 
-    .table>thead>tr>th, .table>tbody>tr>th, .table>tfoot>tr>th, .table>thead>tr>td, .table>tbody>tr>td, .table>tfoot>tr>td{
-       padding: 1px;
-   }
+.table>thead>tr>th, .table>tbody>tr>th, .table>tfoot>tr>th, .table>thead>tr>td, .table>tbody>tr>td, .table>tfoot>tr>td{
+   padding: 1px;
+}
 
 </style>
 <body>
@@ -463,7 +463,7 @@
         </body>
         <!--   Core JS Files   -->
 
-        <script src="{{ asset('js/app.js?v=1.87')}}" type="text/javascript">
+        <script src="{{ asset('js/app.js?v=1.90')}}" type="text/javascript">
 
         </script>
         <script src="{{ asset('js/bootstrap.min.js') }}" type="text/javascript">
