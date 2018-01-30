1396f2fd4bf30b28cf5ea35a4b50f3066c8a5732
fahrizalrahman <fahrizal95rhm@gmail.com>
2018-01-30 10:06:08 +0700

    buat form tambah kas pintas pembayarn hutang

--- resources/assets/js/components/pembayaran_hutang/PembayaranHutangEdit.vue
+++ resources/assets/js/components/pembayaran_hutang/PembayaranHutangEdit.vue
@@ -58,6 +58,64 @@
                 <li class="active"> Edit Pembayaran Hutang</li>
             </ul>
 
+        <div class="modal" id="modal_tambah_kas" role="dialog" data-backdrop=""> 
+                <div class="modal-dialog"> 
+                  <!-- Modal content--> 
+                  <div class="modal-content"> 
+                    <div class="modal-header"> 
+                      <button type="button" class="close"  v-on:click="closeModalTambahKas()" v-shortkey.push="['esc']" @shortkey="closeModalTambahKas()"> &times;</button> 
+                      <h4 class="modal-title"> 
+                        <div class="alert-icon"> 
+                          <b>Silahkan Isi Kas!</b> 
+                        </div> 
+                      </h4> 
+                    </div> 
+                    <div class="modal-body">
+                      <form v-on:submit.prevent="saveFormKas()" class="form-horizontal"> 
+                        <div class="form-group">
+                          <label for="kode_kas" class="col-md-3 control-label">Kode Kas</label>
+                          <div class="col-md-9">
+                            <input class="form-control" autocomplete="off" placeholder="Kode Kas" v-model="tambahKas.kode_kas" type="text" name="kode_kas" id="kode_kas"  autofocus="">
+                            <span v-if="errors.kode_kas" id="kode_kas_error" class="label label-danger">{{ errors.kode_kas[0] }}</span>
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <label for="nama_kas" class="col-md-3 control-label">Nama Kas</label>
+                          <div class="col-md-9">
+                            <input class="form-control" autocomplete="off" placeholder="Nama Kas" v-model="tambahKas.nama_kas" type="text" name="nama_kas" id="nama_kas"  >
+                            <span v-if="errors.nama_kas" id="nama_kas_error" class="label label-danger">{{ errors.nama_kas[0] }}</span>
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <label for="nama_kas" class="col-md-3 control-label">Tampil Transaksi</label>
+                          <div class="togglebutton col-md-9">
+                            <label>
+                              <b>No</b>  <input type="checkbox" v-model="tambahKas.status_kas" value="1" name="status_kas" id="status_kas"><b>Yes</b>
+                            </label>
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <label for="nama_kas" class="col-md-3 control-label">Default Kas</label>
+                          <div class="togglebutton col-md-9">
+                            <label>
+                              <b>No</b>  <input type="checkbox" v-on:change="defaultKas()" v-model="tambahKas.default_kas" value="1" name="default_kas" id="default_kas"><b>Yes</b>
+                            </label>
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <div class="col-md-9 col-md-offset-3">
+                            <p style="color: red; font-style: italic;">*Note : Hanya 1 Kas yang dijadikan default.</p>
+                            <button class="btn btn-primary" id="btnSimpanKas" type="submit"><i class="material-icons">send</i> Submit</button>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                    <div class="modal-footer">  
+                    </div> 
+                  </div>       
+                </div> 
+              </div> 
+              <!-- / MODAL TOMBOL SELESAI --> 
 
           <div class="modal" id="modal_pilih_hutang" role="dialog" data-backdrop=""> 
                   <div class="modal-dialog modal-lg"> 
@@ -409,13 +467,14 @@
             return {
             errors: [],
             suplier: [],
-            cara_bayar:[],
+            kas:[],
             tbs_pembayaran_hutang: [],
             tbsPembayaranHutangData : {},
             dataSuplierHutang : [],
             dataSuplierHutangData: {},
                  url : window.location.origin+(window.location.pathname).replace("dashboard", "pembayaran-hutang"),
                  url_kas : window.location.origin+(window.location.pathname).replace("dashboard", "penjualan"),
+                 url_tambah_kas : window.location.origin+(window.location.pathname).replace("dashboard", "kas"),
                 placeholder_suplier: {
                 placeholder: '--PILIH SUPLIER (F1)--',
                 sortField: 'text',
@@ -451,7 +510,13 @@
                 tanggal: new Date,
                 keterangan: '',
             },
-           separator: {
+            tambahKas: {
+              kode_kas : '',
+              nama_kas : '',
+              status_kas : 0,
+              default_kas : 0
+            },
+            separator: {
               decimal: ',',
               thousands: '.',
               prefix: '',
@@ -472,9 +537,6 @@
             app.dataSuplier();
             app.dataKas(); 
             app.getResults();
-            app.dataSupplierHutang();
-            app.getSubtotalTbs();
-
         },
         watch: {
         // whenever question changes, this function will run
@@ -576,14 +638,12 @@ methods: {
         dataKas() {
         var app = this;
         axios.get(app.url+'/pilih-kas').then(function (resp) {
-            app.kas = resp.data;   
+            app.kas = resp.data;  
             $.each(resp.data, function (i, item) {
                 if (resp.data[i].default_kas == 1) {
                     app.inputPembayaranHutang.kas = resp.data[i].id 
                 }
-
             });
-
         })
         .catch(function (resp) {
 
@@ -867,7 +927,7 @@ methods: {
     },
        bayarPembayaranHutang(){        
         $("#modal_selesai").show();
-        this.$refs.keterangan.$el.focus()
+        
     },
     selesaiPembayaranHutang(){
         this.$swal({
@@ -906,14 +966,62 @@ methods: {
                 app.loading = false;                
                 app.$router.replace('/pembayaran-hutang');
             }
-        })
-        .catch(function (resp) {
-            console.log(resp);              
-            app.loading = false;
-            alert("Tidak dapat Menyelesaikan Transaksi Pembayaran Hutang");        
+            })
+            .catch(function (resp) {
+                console.log(resp);              
+                app.loading = false;
+                alert("Tidak dapat Menyelesaikan Transaksi Pembayaran Hutang");        
+                app.errors = resp.response.data.errors;
+            });
+        },
+          tambahModalKas(){
+         var app = this;
+          $("#modal_tambah_kas").show();
+           $("#modal_selesai").hide();
+        },
+        saveFormKas() {
+          var app = this;
+          var newkas = app.tambahKas;
+          axios.post(app.url_tambah_kas, newkas)
+          .then(function (resp) {
+            app.message = 'Menambah '+ app.tambahKas.nama_kas;
+            app.alert(app.message);
+            app.tambahKas.kode_kas = ''
+            app.tambahKas.nama_kas = ''
+            app.tambahKas.status_kas = 0
+            app.tambahKas.default_kas = 0
+            app.errors = '';
+            $("#modal_tambah_kas").hide();
+             $("#modal_selesai").show();
+             app.dataKas();
+          })
+          .catch(function (resp) {
+            app.success = false;
             app.errors = resp.response.data.errors;
-        });
-    },
+          });
+        },
+        defaultKas() {
+          var app = this;
+          var toogle = app.tambahKas.default_kas;
+
+          if (toogle == true) {
+            app = this;
+            app.$swal({
+              title: "Konfirmasi",
+              text: "Apakah Anda Yakin Ingin Mengubah Kas Utama ?",
+              icon: "warning",
+              buttons: true,
+              dangerMode: true,
+            })
+            .then((confirm) => {
+              if (confirm) {
+                toogle.prop('checked', true);
+              } else {
+                toogle.prop('checked', false);
+              }
+            });
+          }  
+        },
        closeModalX(){
         $("#modal_pilih_hutang").hide(); 
         },
@@ -926,6 +1034,10 @@ methods: {
         },
          closeModalSelesai(){
           $("#modal_selesai").hide(); 
+        },
+        closeModalTambahKas(){
+        $("#modal_selesai").show();
+        $("#modal_tambah_kas").hide();    
         },
            alertTbs(pesan) {
               this.$swal({
