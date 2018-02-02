fdfcf92c709c536afd9fccf2fdc8054d6114210b
khofiq <khoirulgeong123@gmail.com>
2018-02-02 14:24:58 +0700

    proses verifikasi dengan email pada pelanggan

--- app/User.php
+++ app/User.php
@@ -2,8 +2,9 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Mail;
 use Laratrust\Traits\LaratrustUserTrait;
 use Yajra\Auditable\AuditableTrait;
 
@@ -13,14 +14,16 @@ class User extends Authenticatable
     use LaratrustUserTrait;
     use AuditableTrait;
 
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
+    // protected $casts = [
+    //     'is_verified' => 'boolean',
+    // ];
     protected $fillable = [
-        'name', 'email', 'password','alamat','status_konfirmasi','tipe_user', 'wilayah', 'komunitas', 'no_telp','tgl_lahir','nama_bank', 'no_rekening','an_rekening','id_warung','kode_verifikasi'
+        'name', 'email', 'password', 'alamat', 'status_konfirmasi', 'tipe_user', 'wilayah', 'komunitas', 'no_telp', 'tgl_lahir', 'nama_bank', 'no_rekening', 'an_rekening', 'id_warung', 'kode_verifikasi',
     ];
 
     /**
@@ -34,12 +37,30 @@ class User extends Authenticatable
 
     public function role()
     {
-        return $this->hasOne('App\Otoritas','user_id','id');
+        return $this->hasOne('App\Otoritas', 'user_id', 'id');
     }
-           // Specify Slack Webhook URL to route notifications to 
-    public function routeNotificationForSlack() {
+    // Specify Slack Webhook URL to route notifications to
+    public function routeNotificationForSlack()
+    {
         return 'https://hooks.slack.com/services/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';
     }
 
+    public function sendVerification()
+    {
+        $user                     = $this;
+        $token                    = str_random(40);
+        $user->verification_token = $token;
+        $user->save();
+        Mail::send('auth.emails.verification', compact('user', 'token'), function ($message) use ($user) {
+            $message->to($user->email, $user->name)->subject('Konfirmasi Registrasi Akun Topos Anda');
+        });
+    }
+
+    public function verifyEmail()
+    {
+        $this->status_konfirmasi  = 1;
+        $this->verification_token = null;
+        $this->save();
+    }
 
 }
