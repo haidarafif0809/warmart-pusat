66a2ba3cd44324dd0c138fd2479d999f2cb3958e
khofiq <khoirulgeong123@gmail.com>
2018-02-14 14:00:50 +0700

    membuat redirect dan alert saat daftar pelanggan sesuai setting verifikasi

--- app/Http/Middleware/UserShouldVerified.php
+++ app/Http/Middleware/UserShouldVerified.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\SettingVerifikasi;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
@@ -17,27 +18,48 @@ class UserShouldVerified
      */
     public function handle($request, Closure $next)
     {
-        $response = $next($request);
-        if (Auth::check() && !Auth::user()->status_konfirmasi) {
+        $response           = $next($request);
+        $setting_verifikasi = SettingVerifikasi::select()->first();
+        if ($setting_verifikasi->email != 1 && $setting_verifikasi->no_telp == 1) {
+            if (Auth::check() && !Auth::user()->status_konfirmasi) {
 
-            Auth::logout();
+                Auth::logout();
 
-            Session::flash("flash_notification", [
-                "alert"   => 'warning',
-                "icon"    => 'error_outline',
-                "judul"   => 'PERHATIAN',
-                "message" => 'Silakan input nomor verifikasi yang terkirim melalui SMS ke no ' . $request['no_telp'] . '',
-            ]);
-            return redirect('/kirim-kode-verifikasi?nomor=' . $request['no_telp'] . '&status=0');
-        } elseif (Auth::check() && !Auth::user()->status_konfirmasi) {
-            Auth::logout();
-            Session::flash("flash_notification", [
-                "alert"   => "warning",
-                "icon"    => 'error_outline',
-                "judul"   => 'PERHATIAN',
-                "message" => 'Kami mengirimkan link verifikasi melalui Email ke alamat email ' . $request['email'] . 'Periksa email Anda sekarang .',
-            ]);
-            return redirect('/login');
+                Session::flash("flash_notification", [
+                    "alert"   => 'warning',
+                    "icon"    => 'error_outline',
+                    "judul"   => 'PERHATIAN',
+                    "message" => 'Silakan input nomor verifikasi yang terkirim melalui SMS ke no ' . $request['no_telp'] . '',
+                ]);
+                return redirect('/kirim-kode-verifikasi?nomor=' . $request['no_telp'] . '&status=0');
+                # code...
+            }
+        } elseif ($setting_verifikasi->email == 1 && $setting_verifikasi->no_telp == 1) {
+            if (Auth::check() && !Auth::user()->status_konfirmasi) {
+
+                Auth::logout();
+
+                Session::flash("flash_notification", [
+                    "alert"   => 'warning',
+                    "icon"    => 'error_outline',
+                    "judul"   => 'PERHATIAN',
+                    "message" => 'Silakan input nomor verifikasi yang terkirim melalui SMS ke no ' . $request['no_telp'] . ' . Atau verifikasi Melalui link yang di kirim ke email : ' . $request['email'] . ' .',
+                ]);
+                return redirect('/kirim-kode-verifikasi?nomor=' . $request['no_telp'] . '&status=0');
+                # code...
+            }
+        } elseif ($setting_verifikasi->email == 1 && $setting_verifikasi->no_telp != 1) {
+            if (Auth::check() && !Auth::user()->status_konfirmasi) {
+                Auth::logout();
+                Session::flash("flash_notification", [
+                    "alert"   => "warning",
+                    "icon"    => 'error_outline',
+                    "judul"   => 'PERHATIAN',
+                    "message" => 'Kami mengirimkan link verifikasi melalui Email ke alamat email ' . $request['email'] . ' . Periksa email Anda sekarang .',
+                ]);
+                return redirect('/login');
+                # code...
+            }
         }
 
         return $response;
