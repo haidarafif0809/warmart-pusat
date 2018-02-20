345d6c37425548b664649e1187d6bc30dedc9304
RindangRamadhan <rindangramadhan10@gmail.com>
2018-02-20 17:05:07 +0700

    Stok Opname
    - Perbaiki Proses Menampilkan dan Mencari data Stok Opname
    - Create Sttok Opname

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -6,8 +6,8 @@ use App\Barang;
 use App\Hpp;
 use App\KategoriBarang;
 use App\Satuan;
-use App\User;
 use App\SettingAplikasi;
+use App\User;
 use Auth;
 use File;
 use Illuminate\Http\Request;
@@ -79,7 +79,7 @@ class BarangController extends Controller
             array_push($array_produk, [
                 'produk'        => $produk,
                 'harga_jual'    => $this->tandaPemisahTitik($produk->harga_jual),
-                'harga_jual2'    => $this->tandaPemisahTitik($produk->harga_jual2),
+                'harga_jual2'   => $this->tandaPemisahTitik($produk->harga_jual2),
                 'harga_beli'    => $this->tandaPemisahTitik($produk->harga_beli),
                 'nama_produk'   => $produk->NamaProduk,
                 'status_produk' => $status_produk,
@@ -95,11 +95,11 @@ class BarangController extends Controller
     {
         $data_produk = Barang::with(['satuan', 'kategori_barang'])->where('id_warung', Auth::user()->id_warung)->where(function ($query) use ($request) {
             $query->orwhere('kode_barang', 'LIKE', '%' . $request->search . '%')
-            ->orwhere('kode_barcode', 'LIKE', '%' . $request->search . '%')
-            ->orwhere('nama_barang', 'LIKE', '%' . $request->search . '%')
-            ->orwhere('harga_beli', 'LIKE', '%' . $request->search . '%')
-            ->orwhere('harga_jual', 'LIKE', '%' . $request->search . '%')
-            ->orwhere('harga_jual2', 'LIKE', '%' . $request->search . '%');
+                ->orwhere('kode_barcode', 'LIKE', '%' . $request->search . '%')
+                ->orwhere('nama_barang', 'LIKE', '%' . $request->search . '%')
+                ->orwhere('harga_beli', 'LIKE', '%' . $request->search . '%')
+                ->orwhere('harga_jual', 'LIKE', '%' . $request->search . '%')
+                ->orwhere('harga_jual2', 'LIKE', '%' . $request->search . '%');
         })->orderBy('id', 'desc')->paginate(10);
         $array_produk = array();
         foreach ($data_produk as $produk) {
@@ -108,7 +108,7 @@ class BarangController extends Controller
             array_push($array_produk, [
                 'produk'        => $produk,
                 'harga_jual'    => $this->tandaPemisahTitik($produk->harga_jual),
-                'harga_jual2'    => $this->tandaPemisahTitik($produk->harga_jual2),
+                'harga_jual2'   => $this->tandaPemisahTitik($produk->harga_jual2),
                 'harga_beli'    => $this->tandaPemisahTitik($produk->harga_beli),
                 'nama_produk'   => $produk->NamaProduk,
                 'status_produk' => $status_produk,
@@ -185,51 +185,51 @@ class BarangController extends Controller
                 'foto'               => 'image|max:3072',
             ]);
 
-            if ($request->perkiraan_berat == "" OR $request->perkiraan_berat == 0) {
-               $perkiraan_berat = 1000;
-           }else{
-            $perkiraan_berat = $request->perkiraan_berat;
-        }
+            if ($request->perkiraan_berat == "" or $request->perkiraan_berat == 0) {
+                $perkiraan_berat = 1000;
+            } else {
+                $perkiraan_berat = $request->perkiraan_berat;
+            }
 
-        $insert_barang = Barang::create([
-            'kode_barang'        => $request->kode_barang,
-            'kode_barcode'       => $request->kode_barcode,
-            'nama_barang'        => strtolower($request->nama_barang),
-            'harga_beli'         => $request->harga_beli,
-            'harga_jual'         => $request->harga_jual,
-            'harga_jual2'         => $request->harga_jual2,
-            'berat'             => $perkiraan_berat,
-            'satuan_id'          => $request->satuan_id,
-            'kategori_barang_id' => $request->kategori_barang_id,
-            'deskripsi_produk'   => $request->deskripsi_produk,
-            'status_aktif'       => $request->status_aktif,
-            'hitung_stok'        => $request->hitung_stok,
-            'konfirmasi_admin'   => 1,
-            'id_warung'          => Auth::user()->id_warung]);
+            $insert_barang = Barang::create([
+                'kode_barang'        => $request->kode_barang,
+                'kode_barcode'       => $request->kode_barcode,
+                'nama_barang'        => strtolower($request->nama_barang),
+                'harga_beli'         => $request->harga_beli,
+                'harga_jual'         => $request->harga_jual,
+                'harga_jual2'        => $request->harga_jual2,
+                'berat'              => $perkiraan_berat,
+                'satuan_id'          => $request->satuan_id,
+                'kategori_barang_id' => $request->kategori_barang_id,
+                'deskripsi_produk'   => $request->deskripsi_produk,
+                'status_aktif'       => $request->status_aktif,
+                'hitung_stok'        => $request->hitung_stok,
+                'konfirmasi_admin'   => 1,
+                'id_warung'          => Auth::user()->id_warung]);
 
-        if ($request->hasFile('foto')) {
-            $foto = $request->file('foto');
+            if ($request->hasFile('foto')) {
+                $foto = $request->file('foto');
 
-            if (is_array($foto) || is_object($foto)) {
+                if (is_array($foto) || is_object($foto)) {
                     // Mengambil file yang diupload
-                $uploaded_foto = $foto;
+                    $uploaded_foto = $foto;
                     // mengambil extension file
-                $extension = $uploaded_foto->getClientOriginalExtension();
+                    $extension = $uploaded_foto->getClientOriginalExtension();
                     // membuat nama file random berikut extension
-                $filename     = str_random(40) . '.' . $extension;
-                $image_resize = Image::make($foto->getRealPath());
-                $image_resize->fit(300);
-                $image_resize->save(public_path('foto_produk/' . $filename));
-                $insert_barang->foto = $filename;
+                    $filename     = str_random(40) . '.' . $extension;
+                    $image_resize = Image::make($foto->getRealPath());
+                    $image_resize->fit(300);
+                    $image_resize->save(public_path('foto_produk/' . $filename));
+                    $insert_barang->foto = $filename;
                     // menyimpan field foto_kamar di database kamar dengan filename yang baru dibuat
-                $insert_barang->save();
+                    $insert_barang->save();
+                }
+
             }
 
         }
 
     }
-
-}
 
     /**
      * Display the specified resource.
@@ -283,8 +283,8 @@ class BarangController extends Controller
                 'nama_barang'        => 'required|max:300',
                 'harga_beli'         => 'required|numeric|digits_between:1,11',
                 'harga_jual'         => 'required|numeric|digits_between:1,11',
-                'harga_jual2'         => 'nullable|numeric|digits_between:1,11',
-                'berat'    => 'nullable|numeric',
+                'harga_jual2'        => 'nullable|numeric|digits_between:1,11',
+                'berat'              => 'nullable|numeric',
                 'kategori_barang_id' => 'required|exists:kategori_barangs,id',
                 'satuan_id'          => 'required|exists:satuans,id',
                 'foto'               => 'image|max:3072',
@@ -302,9 +302,9 @@ class BarangController extends Controller
             } else {
                 $hitung_stok = 0;
             }
-            if ($request->berat == "" OR $request->berat == 0) {
+            if ($request->berat == "" or $request->berat == 0) {
                 $berat = 1000;
-            }else{
+            } else {
                 $berat = $request->berat;
             }
             $update_barang->update([
@@ -313,8 +313,8 @@ class BarangController extends Controller
                 'nama_barang'        => strtolower($request->nama_barang),
                 'harga_beli'         => $request->harga_beli,
                 'harga_jual'         => $request->harga_jual,
-                'harga_jual2'         => $request->harga_jual2,
-                'berat'             => $berat,
+                'harga_jual2'        => $request->harga_jual2,
+                'berat'              => $berat,
                 'satuan_id'          => $request->satuan_id,
                 'kategori_barang_id' => $request->kategori_barang_id,
                 'deskripsi_produk'   => $request->deskripsi_produk,
@@ -397,15 +397,15 @@ class BarangController extends Controller
     {
         $lihat_deskripsi_produk = Barang::find($id);
         $nama_produk            = $lihat_deskripsi_produk->nama_barang;
-        $setting_aplikasi = SettingAplikasi::select('tipe_aplikasi')->first();
-        $foto_latar_belakang = "background-image: asset('image/background2.jpg');";
-        $agent = new Agent();
+        $setting_aplikasi       = SettingAplikasi::select('tipe_aplikasi')->first();
+        $foto_latar_belakang    = "background-image: asset('image/background2.jpg');";
+        $agent                  = new Agent();
 
         if ($lihat_deskripsi_produk->id_warung != Auth::user()->id_warung) {
             Auth::logout();
             return response()->view('error.403');
         } else {
-            return view('barang.lihat_deskripsi_produk', ['id' => $id, 'lihat_deskripsi_produk' => $lihat_deskripsi_produk, 'nama_produk' => $nama_produk,'setting_aplikasi'=> $setting_aplikasi,'foto_latar_belakang'=>$foto_latar_belakang,'agent' => $agent]);
+            return view('barang.lihat_deskripsi_produk', ['id' => $id, 'lihat_deskripsi_produk' => $lihat_deskripsi_produk, 'nama_produk' => $nama_produk, 'setting_aplikasi' => $setting_aplikasi, 'foto_latar_belakang' => $foto_latar_belakang, 'agent' => $agent]);
         }
     }
 
@@ -418,7 +418,7 @@ class BarangController extends Controller
                 'id'          => $produks->id,
                 'nama_produk' => title_case($produks->nama_barang),
                 'kode_produk' => $produks->kode_barang,
-                'barcode' => $produks->kode_barcode,
+                'barcode'     => $produks->kode_barcode,
                 'hitung_stok' => $produks->hitung_stok,
                 'produk'      => $produks->id . "|" . title_case($produks->nama_barang) . "|" . $produks->harga_beli . "|" . $produks->harga_jual . "|" . $produks->satuan_id . "|" . $produks->harga_jual2]);
 
--- resources/assets/js/components/stok_opname/StokOpnameIndex.vue
+++ resources/assets/js/components/stok_opname/StokOpnameIndex.vue
@@ -7,6 +7,9 @@
     .card-produk{
         background-color:#82B1FF;
     }
+    .text-kanan{
+        text-align:right;
+    }
 </style>
 
 <template>  
@@ -25,14 +28,14 @@
                             <button type="button" class="close"  v-on:click="closeModalJumlahProduk()" v-shortkey.push="['f9']" @shortkey="closeModalJumlahProduk()"> &times;</button> 
                         </div>
 
-                        <form class="form-horizontal" v-on:submit.prevent="submitProdukPenjualan(inpuStokOpname.jumlah_produk)"> 
+                        <form class="form-horizontal" v-on:submit.prevent="submitProduk(inputStokOpname.jumlah_produk)"> 
                             <div class="modal-body text-center">
-                                <h3><b>{{inpuStokOpname.nama_produk}}</b> </h3>
-                                <input class="form-control" type="number" v-model="inpuStokOpname.jumlah_produk" placeholder="Isi Jumlah Produk" name="jumlah_produk" id="jumlah_produk" ref="jumlah_produk" autocomplete="off" step="0.01">
+                                <h3><b>{{inputStokOpname.nama_produk}}</b> </h3>
+                                <input class="form-control" type="number" v-model="inputStokOpname.jumlah_produk" placeholder="Isi Jumlah Produk" name="jumlah_produk" id="jumlah_produk" ref="jumlah_produk" autocomplete="off" step="0.01">
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-simple"   v-on:click="closeModalJumlahProduk()" v-shortkey.push="['f9']" @shortkey="closeModalJumlahProduk()">Close(F9)</button>
-                                <button type="button" class="btn btn-info btn-lg"   v-on:click="submitProdukPenjualan(inpuStokOpname.jumlah_produk)">Tambah</button>
+                                <button type="button" class="btn btn-info btn-lg"   v-on:click="submitProduk(inputStokOpname.jumlah_produk)">Tambah</button>
                             </div>
                         </form>
                         
@@ -52,10 +55,10 @@
                         <div class="card card-produk" style="margin-bottom: 1px; margin-top: 1px;">
 
                             <div class="form-group" style="margin-right: 10px; margin-left: 10px;">
-                                <selectize-component v-model="inpuStokOpname.produk" :settings="placeholder_produk" id="produk" ref='produk' >
+                                <selectize-component v-model="inputStokOpname.produk" :settings="placeholder_produk" id="produk" ref='produk' >
                                     <option v-for="produks, index in produk" v-bind:value="produks.produk">{{produks.barcode}} || {{produks.kode_produk}} || {{ produks.nama_produk }}</option>
                                 </selectize-component>
-                                <input class="form-control" type="hidden"  v-model="inpuStokOpname.produk"  name="produk" id="produk" v-shortkey="['f1']" @shortkey="openSelectizeProduk()">
+                                <input class="form-control" type="hidden"  v-model="inputStokOpname.produk"  name="produk" id="produk" v-shortkey="['f1']" @shortkey="openSelectizeProduk()">
                             </div>  
                         </div>
                     </div>                   
@@ -72,12 +75,12 @@
 
                                         <th>No. Faktur</th>
                                         <th>Produk</th>
-                                        <th>Stok Komputer</th>
-                                        <th>Stok Fisik</th>
-                                        <th>Selisih Fisik</th>
-                                        <th>Selisih Harga</th>
+                                        <th class="text-kanan">Stok Komputer</th>
+                                        <th class="text-kanan">Stok Fisik</th>
+                                        <th class="text-kanan">Selisih Fisik</th>
+                                        <th class="text-kanan">Harga</th>
+                                        <th class="text-kanan">Selisih Harga</th>
                                         <th>Petugas</th>
-                                        <th>Keterangan</th>
                                         <th>Waktu</th>
                                         <th>Aksi</th>
 
@@ -85,23 +88,23 @@
                                 </thead>
                                 <tbody v-if="stokOpname.length"  class="data-ada">
                                     <tr v-for="stokOpname, index in stokOpname" >
-                                        <td>{{ stokOpname.no_faktur }}</td>
-                                        <td>{{ stokOpname.no_faktur }}</td>
-                                        <td>{{ stokOpname.no_faktur }}</td>
-                                        <td>{{ stokOpname.no_faktur }}</td>
-                                        <td>{{ stokOpname.no_faktur }}</td>
-                                        <td>{{ stokOpname.no_faktur }}</td>
-                                        <td>{{ stokOpname.no_faktur }}</td>
-                                        <td>{{ stokOpname.no_faktur }}</td>
-                                        <td>{{ stokOpname.no_faktur }}</td>
+                                        <td>{{ stokOpname.stok_opname.no_faktur }}</td>
+                                        <td>{{ stokOpname.nama_produk }}</td>
+                                        <td align="right">{{ stokOpname.stok_opname.stok_sekarang | pemisahTitik}}</td>
+                                        <td align="right">{{ stokOpname.stok_opname.jumlah_fisik | pemisahTitik}}</td>
+                                        <td align="right">{{ stokOpname.stok_opname.selisih_fisik | pemisahTitik}}</td>
+                                        <td align="right">{{ stokOpname.stok_opname.harga | pemisahTitik}}</td>
+                                        <td align="right">{{ stokOpname.stok_opname.total | pemisahTitik}}</td>
+                                        <td>{{ stokOpname.stok_opname.petugas }}</td>
+                                        <td>{{ stokOpname.stok_opname.created_at | tanggal}}</td>
                                         <td>
-                                            <router-link :to="{name: 'editstokOpname', params: {id: stokOpname.id}}" class="btn btn-xs btn-default" v-bind:id="'edit-' + stokOpname.id" > Edit
+                                            <router-link :to="{name: 'editstokOpname.stok_opname', params: {id: stokOpname.stok_opname.id}}" class="btn btn-xs btn-default" v-bind:id="'edit-' + stokOpname.stok_opname.id" > Edit
                                             </router-link>
 
-                                            <a v-if="stokOpname.status_transaksi == 0" href="#" class="btn btn-xs btn-danger" v-bind:id="'delete-' + stokOpname.id" v-on:click="deleteEntry(stokOpname.id, index,stokOpname.nama_kategori_transaksi)">Delete
+                                            <a v-if="stokOpname.stok_opname.status_transaksi == 0" href="#" class="btn btn-xs btn-danger" v-bind:id="'delete-' + stokOpname.stok_opname.id" v-on:click="deleteEntry(stokOpname.stok_opname.id, index,stokOpname.stok_opname.nama_kategori_transaksi)">Delete
                                             </a>
 
-                                            <a v-else href="#" class="btn btn-xs btn-danger" v-bind:id="'delete-' + stokOpname.id" v-on:click="gagalHapus(stokOpname.id, index,stokOpname.nama_kategori_transaksi)">Delete
+                                            <a v-else href="#" class="btn btn-xs btn-danger" v-bind:id="'delete-' + stokOpname.stok_opname.id" v-on:click="gagalHapus(stokOpname.stok_opname.id, index,stokOpname.stok_opname.nama_kategori_transaksi)">Delete
                                             </a>
                                         </td>
                                     </tr>
@@ -130,9 +133,11 @@
             return {
                 stokOpname: [],
                 stokOpnameData: {},
-                inpuStokOpname: {
+                inputStokOpname: {
                     produk: '',
                     nama_produk: '',
+                    jumlah_produk: '',
+                    harga_produk: '',
                 },
                 placeholder_produk:{
                     placeholder: 'Cari Produk (F1) ...'
@@ -147,11 +152,22 @@
             app.$store.dispatch('LOAD_PRODUK_LIST')
             app.getResults();
         },
+        filters: {
+            pemisahTitik: function (value) {
+                var angka = [value];
+                var numberFormat = new Intl.NumberFormat('es-ES');
+                var formatted = angka.map(numberFormat.format);
+                return formatted.join('; ');
+            },
+            tanggal: function (value) {
+                return moment(String(value)).format('DD/MM/YYYY hh:mm')
+            }
+        },
         watch: {
             pencarian: function (newQuestion) {
                 this.getHasilPencarian()  
             },
-            'inpuStokOpname.produk': function(){
+            'inputStokOpname.produk': function(){
                 this.pilihProduk()
             }
         },
@@ -171,6 +187,7 @@
                 }
                 axios.get(app.url+'/view?page='+page)
                 .then(function (resp) {
+                    console.log(resp.data)
                     app.stokOpname = resp.data.data;
                     app.stokOpnameData = resp.data;
                     app.loading = false;
@@ -199,9 +216,9 @@
                 });
             },
             pilihProduk() {
-                if (this.inpuStokOpname.produk != '') {
+                if (this.inputStokOpname.produk != '') {
                     var app = this;
-                    var produk = app.inpuStokOpname.produk.split("|");
+                    var produk = app.inputStokOpname.produk.split("|");
                     var nama_produk = produk[1];
 
                     this.inputJumlahProduk(nama_produk);
@@ -209,15 +226,61 @@
             },
             inputJumlahProduk(nama_produk){
                 var app = this
-                app.inpuStokOpname.nama_produk = nama_produk;
+                app.inputStokOpname.nama_produk = nama_produk;
                 $("#modalJumlahProduk").show();
                 app.$refs.jumlah_produk.focus(); 
             },
+            submitProduk(value){
+
+                if (value == '') {
+                    this.$swal("Jumlah Produk Tidak Boleh Kosong!").then((value) => {
+                        this.$refs.jumlah_produk.focus(); 
+                    });
+                }else{
+                    var app = this;
+                    var data_produk = app.inputStokOpname.produk.split("|");
+                    var nama_produk = data_produk[1];
+                    app.inputStokOpname.produk = data_produk[0];
+                    app.inputStokOpname.harga_produk = data_produk[2];
+                    var newInputStokOpname = app.inputStokOpname;
+                    app.loading = true;
+                    axios.post(app.url, newInputStokOpname)
+                    .then(function (resp) {
+
+                        app.alert("Menambahkan Produk "+nama_produk)      
+                        app.getResults()
+                        app.loading = false
+                        app.inputStokOpname.jumlah_produk = ''
+                        app.inputStokOpname.nama_produk = ''
+                        app.inputStokOpname.produk = ''
+                        $("#modalJumlahProduk").hide();
+                    })
+                    .catch(function (resp) {
+                      console.log(resp);                  
+                      app.loading = false;
+                      app.inputStokOpname.jumlah_produk = ''
+                      app.inputStokOpname.nama_produk = ''
+                      app.inputStokOpname.produk = ''
+                      $("#modalJumlahProduk").hide();
+                      alert("Tidak Dapat Menambahkan Produk "+nama_produk);
+                  });
+                }
+            },
             closeModalJumlahProduk(){  
                 $("#modalJumlahProduk").hide();
-                this.inpuStokOpname.produk = '';
+                this.inputStokOpname.produk = '';
                 this.openSelectizeProduk();
             },
-        }
-    }
+            alert(pesan) {
+              this.$swal({
+                title: "Berhasil ",
+                text: pesan,
+                icon: "success",
+                buttons: false,
+                timer: 1000,
+
+            });
+          }
+      }
+  }
 </script>
