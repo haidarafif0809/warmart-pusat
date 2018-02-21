c2c57deb71aa5be4a64c2549eaaa432eafb63d9f
Riko Pernando <rikopernando96@gmail.com>
2018-02-21 15:19:26 +0700

    Perbaiki Logik perubahan Harga di create pembelian
    Perbaiki Logik perubahan Harga di create pembelian

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -457,7 +457,7 @@ $judul_warung = \App\SettingFooter::select()->first()->judul_warung;
 </body>
 <!--   Core JS Files   -->
 
-<script src="{{ asset('js/app.js?v=1.63')}}" type="text/javascript">
+<script src="{{ asset('js/app.js?v=1.65')}}" type="text/javascript">
 
 </script>
 <script src="{{ asset('js/bootstrap.min.js') }}" type="text/javascript">
