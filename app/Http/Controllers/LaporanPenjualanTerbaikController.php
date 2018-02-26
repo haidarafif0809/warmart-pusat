fed9f4217775a1c74c06020c97efaef3d7ed2779
fahrizalrahman <fahrizal95rhm@gmail.com>
2018-02-26 15:12:33 +0700

    laporan grafik penjualan terbaik (review )
    - judul tooltip dihapus 
    - nama barang menggunakan title case

--- app/Http/Controllers/LaporanPenjualanTerbaikController.php
+++ app/Http/Controllers/LaporanPenjualanTerbaikController.php
@@ -44,7 +44,7 @@ class LaporanPenjualanTerbaikController extends Controller
 
 	           $laporan_penjualan_terbaik = DetailPenjualanPos::penjualanTerbaik($request)->groupBy('id_produk')->orderBy('jumlah_produk', 'desc')->limit($item_tampil_terbaik)->get();
 	           foreach ($laporan_penjualan_terbaik as $laporan_penjualan_terbaiks) {
-			        $respons['nama_barang'][] =  $laporan_penjualan_terbaiks->nama_barang;
+			        $respons['nama_barang'][] =  title_case($laporan_penjualan_terbaiks->nama_barang);
 		           $respons['jumlah_produk'][]     = $laporan_penjualan_terbaiks->jumlah_produk;   
 		           $respons['color'][]      = $this->random_color();  
 	       }
@@ -69,7 +69,7 @@ class LaporanPenjualanTerbaikController extends Controller
                 
 	           $laporan_penjualan_terbaik = DetailPenjualan::penjualanTerbaik($request)->groupBy('id_produk')->orderBy('jumlah_produk', 'desc')->limit($item_tampil_terbaik)->get();
 	           foreach ($laporan_penjualan_terbaik as $laporan_penjualan_terbaiks) {
-			        $respons['nama_barang'][] =  $laporan_penjualan_terbaiks->nama_barang;
+			        $respons['nama_barang'][] =  title_case($laporan_penjualan_terbaiks->nama_barang);
 		           $respons['jumlah_produk'][]     = $laporan_penjualan_terbaiks->jumlah_produk;   
 		           $respons['color'][]      = $this->random_color();  
 	       }
