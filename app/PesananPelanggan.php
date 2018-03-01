6c1f0c84adbd9facbef478dab7160c7c4f1b26ac
Riko Pernando <rikopernando96@gmail.com>
2018-03-01 10:36:33 +0700

    Membuat tabel kode unik transfer di pesanan pelanggan, buat fungsi untuk mengetahui kode unik dan masukan kode unik ke data base
    Membuat tabel kode unik transfer di pesanan pelanggan, buat fungsi untuk mengetahui kode unik dan masukan kode unik ke data base

--- app/PesananPelanggan.php
+++ app/PesananPelanggan.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 
 class PesananPelanggan extends Model
 {
-    protected $fillable = ['id_pelanggan', 'nama_pemesan', 'no_telp_pemesan', 'alamat_pemesan', 'jumlah_produk', 'subtotal', 'konfirmasi_pesanan', 'id_warung','kurir','layanan_kurir','metode_pembayaran','biaya_kirim','bank_transfer'];
+    protected $fillable = ['id_pelanggan', 'nama_pemesan', 'no_telp_pemesan', 'alamat_pemesan', 'jumlah_produk', 'subtotal', 'konfirmasi_pesanan', 'id_warung','kurir','layanan_kurir','metode_pembayaran','biaya_kirim','bank_transfer','kode_unik_transfer'];
 
     // relasi ke pelanggan
     public function pelanggan()
@@ -28,13 +28,33 @@ class PesananPelanggan extends Model
         return $date_terbalik;
     }
 
-            public function scopeQueryCetak($query, $id)
+    public function scopeQueryCetak($query, $id)
     {
         $query->select('w.name AS nama_warung', 'w.alamat AS alamat_warung', 'p.name AS pelanggan', 'pesanan_pelanggans.subtotal AS total', DB::raw('DATE_FORMAT(pesanan_pelanggans.created_at, "%d/%m/%Y %H:%i:%s") as waktu_jual'), 'w.no_telpon AS no_telp_warung','pesanan_pelanggans.id AS id', 'pesanan_pelanggans.id_pelanggan AS id_pelanggan')
-            ->leftJoin('warungs AS w', 'pesanan_pelanggans.id_warung', '=', 'w.id')
-            ->leftJoin('users AS p', 'p.id', '=', 'pesanan_pelanggans.id_pelanggan')
-            ->where('pesanan_pelanggans.id', $id);
+        ->leftJoin('warungs AS w', 'pesanan_pelanggans.id_warung', '=', 'w.id')
+        ->leftJoin('users AS p', 'p.id', '=', 'pesanan_pelanggans.id_pelanggan')
+        ->where('pesanan_pelanggans.id', $id);
         return $query;
+    }
+
+    public static function kodeUnikTransfer(){
+        // untuk mengambil id terakhir dari table pesanan pelanggan => limit 1
+        $pesanan = PesananPelanggan::select('id')->orderBy('id', 'DESC')->limit(1);
+        // jika data masih kosong
+        if ($pesanan->count() == 0) {
+            // id pesanan berarti sama dengan nol,
+            // id pesanan = nol + 1
+            $id_pesanan = $pesanan->count() + 1;
+        }else{// jika tidak
+            // id pesanan = id pesanan yg terakhir ditambah 1
+            $id_pesanan = $pesanan->first()->id + 1;
+        }
+
+        // untuk mengetahui kode unik , kita gunakan modulus 1000 id pesanan
+        // hasil nya akan berurut mulai dari 1 sampai 999 , jika id pesanan nya sudah mencapai 1000,2000.3000 dan sterusnya maka kode unik akan kembali 0 
+        // jka masih bingung, silakan diicoba 
+        return $kode_unik = $id_pesanan % 1000;
+
     }
 
 }
