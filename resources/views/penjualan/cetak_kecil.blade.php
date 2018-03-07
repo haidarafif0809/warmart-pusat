5de89c32baed8ae6fbec41f61c7e1bacc19bdbaa
Riko Pernando <rikopernando96@gmail.com>
2018-03-07 14:21:06 +0700

    Perbaiki layout cetak kecil penjualan
    Perbaiki layout cetak kecil penjualan

--- resources/views/penjualan/cetak_kecil.blade.php
+++ resources/views/penjualan/cetak_kecil.blade.php
@@ -21,22 +21,20 @@
 </head>
 <style type="text/css">
 
-	p{
-		margin-top: 1px; margin-bottom: 1px;
-	}
+p{
+	margin-top: 1px; margin-bottom: 1px;
+}
 
 </style>
 <body>
 
 	<p><b>{{$penjualan->nama_warung}}</b></p>
 	<p><b>{{$penjualan->alamat_warung}}</b></p>
-	<p>======================================</p>
-	<p>Pelanggan : {{$nama_pelanggan}}</p>
 	<p>-------------------------------------------------------------</p>
+	<p>Pelanggan : {{$nama_pelanggan}}</p>
 	<p>No. Transaksi : #{{$penjualan->id}}</p>
-
 	<p>Kasir : {{$penjualan->kasir}}</p>
-	<p>-------------------------------------------------------------</p>	
+	<br>
 
 	<table>
 		<tbody>
@@ -44,6 +42,7 @@
 			@foreach ($detail_penjualan as $detail_penjualans)			
 			<tr><td style="padding: 3px;"> {{title_case($detail_penjualans->produk->nama_barang)}} </td><td style="padding:3px" align="right"> {{number_format($detail_penjualans->harga_produk, 0, ',', '.')}}</td><td  align="right" style="padding:3px">{{$detail_penjualans->jumlah_produk}}</td><td  align="right" style="padding:3px">{{number_format($detail_penjualans->harga_produk * $detail_penjualans->jumlah_produk, 0, ',', '.')}}</td></tr>
 			@endforeach
+			
 			<tr><td style="padding: 3px;"> Subtotal </td><td style="padding:3px" align="right">:</td><td  align="right" style="padding:3px"></td><td  align="right" style="padding:3px">{{number_format($subtotal, 0, ',', '.')}}</td></tr>
 			<tr><td style="padding: 3px;"> Diskon </td><td style="padding:3px" align="right">:</td><td  align="right" style="padding:3px"></td><td  align="right" style="padding:3px">{{number_format($potongan, 0, ',', '.')}}</td></tr>
 			<tr><td style="padding: 3px;"> Total </td><td style="padding:3px" align="right">:</td><td  align="right" style="padding:3px"></td><td  align="right" style="padding:3px">{{number_format($penjualan->total, 0, ',', '.')}}</td></tr>
@@ -52,7 +51,7 @@
 		</tbody>
 	</table>
 	
-	<p>======================================</p>
+	<p>-------------------------------------------------------------</p>
 	<p>{{$penjualan->waktu_jual}}</p>
 
 	<p>Terima Kasih </p>
