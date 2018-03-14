d02defe9c0ab7fdaddd8c1c3fc73bc99f2d12a69
fahrizalrahman <fahrizal95rhm@gmail.com>
2018-03-14 16:04:46 +0700

    laporan penjualan terbaik (data)
    - perbaikan proses tampil terbaik tidak jalan 
    - tambahkan route download excel karena hilang saat conflict

--- routes/web.php
+++ routes/web.php
@@ -770,6 +770,8 @@ Route::post('/laporan-penjualan-terbaik/view-pos-data', 'LaporanPenjualanTerbaik
 Route::post('/laporan-penjualan-terbaik/view-online-data', 'LaporanPenjualanTerbaikController@prosesPenjualanTerbaikOnlineData')->middleware('auth');
 Route::post('/laporan-penjualan-terbaik/pencarian-pos-data', 'LaporanPenjualanTerbaikController@pencarian')->middleware('auth');
 Route::post('/laporan-penjualan-terbaik/pencarian-online-data', 'LaporanPenjualanTerbaikController@pencarianOnline')->middleware('auth');
+Route::get('/laporan-penjualan-terbaik/download-excel/{dari_tanggal}/{sampai_tanggal}/{tampil_terbaik}', 'LaporanPenjualanTerbaikController@downloadExcel')->middleware('auth');
+
 
 // LAPORAN PENJUALAN TERBAIK
 Route::post('/laporan-hutang-beredar/view', 'LaporanHutangBeredarController@prosesHutangBeredar')->middleware('auth');
