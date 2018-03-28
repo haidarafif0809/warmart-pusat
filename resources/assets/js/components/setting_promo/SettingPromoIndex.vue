ed2cbc742340ce14a2e6bbed136fd5dbfeb94ccc
Riko Pernando <rikopernando96@gmail.com>
2018-03-28 17:06:48 +0700

    Implementasikan Otoritas Di Fitur Setting Promo
    Implementasikan Otoritas Di Fitur Setting Promo

--- resources/assets/js/components/setting_promo/SettingPromoIndex.vue
+++ resources/assets/js/components/setting_promo/SettingPromoIndex.vue
@@ -14,12 +14,12 @@
 			</ul>
 			<div class="card">
 				<div class="card-header card-header-icon" data-background-color="purple">
- 					<i class="material-icons">settings_applications</i>
+					<i class="material-icons">settings_applications</i>
 				</div>
 				<div class="card-content">
 					<h4 class="card-title"> Setting Promo</h4>
 					<div class="toolbar">
-						<p> <router-link :to="{name: 'createSettingPromo'}" class="btn btn-primary">Tambah Setting Promo</router-link></p>
+						<router-link :to="{name: 'createSettingPromo'}" class="btn btn-primary" v-if="otoritas.tambah_setting_promo == 1">Tambah Setting Promo</router-link>
 					</div>
 
 					<div class=" table-responsive ">
@@ -37,7 +37,7 @@
 									<th style="text-align:right">Periode Promo</th>
 									<th style="text-align:right">Baner Promo</th>
 									<th style="text-align:right">Status</th>
-									<th style="text-align:right">Aksi</th>
+									<th style="text-align:right" v-if="otoritas.edit_setting_promo == 1 || otoritas.hapus_setting_promo == 1">Aksi</th>
 								</tr>
 							</thead>
 							<tbody v-if="settingPromo.length"  class="data-ada">
@@ -47,36 +47,38 @@
 									<td align="right"> {{ settingPromos.settingpromo.harga_coret | pemisahTitik}}</td>
 									<td align="right"> {{ settingPromos.settingpromo.dari_tanggal | tanggal }} sd {{ settingPromos.settingpromo.sampai_tanggal | tanggal }}</td>
 									<td align="right"><a v-if="settingPromos.settingpromo.baner_promo != undefined" v-bind:href="url_baner_promo+ '/'+settingPromos.settingpromo.baner_promo" target="blank">Lihat Baner</a>
-					                    <p v-else >Tidak Ada Baner</p>
-					                 </td>
-					                 <td align="right" v-if="settingPromos.settingpromo.status == 1"> Aktif </td>
-					                 <td align="right" v-else> Tidak Aktif </td>
+										<p v-else >Tidak Ada Baner</p>
+									</td>
+									<td align="right" v-if="settingPromos.settingpromo.status == 1"> Aktif </td>
+									<td align="right" v-else> Tidak Aktif </td>
 									<td align="right"> 
-									<router-link :to="{name: 'editSettingPromo', params: {id: settingPromos.settingpromo.id_setting_promo }}" class="btn btn-xs btn-default" v-bind:id="'edit-' + settingPromos.settingpromo.id_setting_promo" >
-									Edit 
-									</router-link> <a href="#"
-									class="btn btn-xs btn-danger" v-bind:id="'delete-' + settingPromos.settingpromo.id_setting_promo"
-									v-on:click="deleteEntry(settingPromos.settingpromo.id_setting_promo, index,settingPromos.settingpromo.nama_barang)">
-									Delete
-									</a></td>
-								</tr>
-							</tbody>                    
-							<tbody class="data-tidak-ada" v-else>
-								<tr ><td colspan="5"  class="text-center">Tidak Ada Data</td></tr>
-							</tbody>
-						</table>    
+										<router-link :to="{name: 'editSettingPromo', params: {id: settingPromos.settingpromo.id_setting_promo }}" class="btn btn-xs btn-default" v-bind:id="'edit-' + settingPromos.settingpromo.id_setting_promo" v-if="otoritas.edit_setting_promo == 1">
+											Edit 
+										</router-link> 
+										<a href="#"
+										class="btn btn-xs btn-danger" v-bind:id="'delete-' + settingPromos.settingpromo.id_setting_promo"
+										v-on:click="deleteEntry(settingPromos.settingpromo.id_setting_promo, index,settingPromos.settingpromo.nama_barang)" v-if="otoritas.hapus_setting_promo == 1">
+										Delete
+									</a>
+								</td>
+							</tr>
+						</tbody>                    
+						<tbody class="data-tidak-ada" v-else>
+							<tr ><td colspan="5"  class="text-center">Tidak Ada Data</td></tr>
+						</tbody>
+					</table>    
 
-						<vue-simple-spinner v-if="loading"></vue-simple-spinner>
+					<vue-simple-spinner v-if="loading"></vue-simple-spinner>
 
-						<div align="right"><pagination :data="settingPromoData" v-on:pagination-change-page="getResults" :limit="4"></pagination></div>
-					</div>
-
-
-
+					<div align="right"><pagination :data="settingPromoData" v-on:pagination-change-page="getResults" :limit="4"></pagination></div>
 				</div>
+
+
+
 			</div>
 		</div>
 	</div>
+</div>
 
 </template>
 
@@ -88,6 +90,7 @@ export default {
 			errors: [],
 			settingPromo: [],
 			settingPromoData : {},
+			otoritas: {},
 			url : window.location.origin+(window.location.pathname).replace("dashboard", "setting-promo"),
 			url_baner_promo : window.location.origin+(window.location.pathname).replace("dashboard", "baner_setting_promo"),
 			pencarian: '',
@@ -100,23 +103,23 @@ export default {
 		app.getResults();
 	},
 	watch: {
-    // whenever question changes, this function will run
-    pencarian: function (newQuestion) {
-    	this.getHasilPencarian()
-    	this.loading = true
-   		 }
+// whenever question changes, this function will run
+pencarian: function (newQuestion) {
+	this.getHasilPencarian()
+	this.loading = true
+}
+},
+filters: {
+	pemisahTitik: function (value) {
+		var angka = [value];
+		var numberFormat = new Intl.NumberFormat('es-ES');
+		var formatted = angka.map(numberFormat.format);
+		return formatted.join('; ');
 	},
- 	filters: {
-       pemisahTitik: function (value) {
-        var angka = [value];
-        var numberFormat = new Intl.NumberFormat('es-ES');
-        var formatted = angka.map(numberFormat.format);
-        return formatted.join('; ');
-      },
-      tanggal: function (value) {
-        return moment(String(value)).format('DD/MM/YYYY')
-      }
-    },
+	tanggal: function (value) {
+		return moment(String(value)).format('DD/MM/YYYY')
+	}
+},
 methods: {
 	getResults(page) {
 		var app = this; 
@@ -127,6 +130,7 @@ methods: {
 		.then(function (resp) {
 			app.settingPromo = resp.data.data;
 			app.settingPromoData = resp.data;
+			app.otoritas = resp.data.otoritas.original;
 			app.loading = false;
 			app.seen = true;
 		})
@@ -146,6 +150,7 @@ methods: {
 		.then(function (resp) {
 			app.settingPromo = resp.data.data;
 			app.settingPromoData = resp.data;
+			app.otoritas = resp.data.otoritas.original;
 			app.loading = false;
 			app.seen = true;
 		})
@@ -155,38 +160,38 @@ methods: {
 		});
 	},
 	alert(pesan) {
-          this.$swal({
-            title: "Berhasil Menghapus Promo!",
-            text: pesan,
-            icon: "success",
-          });
-        },
+		this.$swal({
+			title: "Berhasil Menghapus Promo!",
+			text: pesan,
+			icon: "success",
+		});
+	},
 	deleteEntry(id, index,nama_barang) {
-          this.$swal({
-            title: "Konfirmasi Hapus",
-            text : "Yakin Ingin Menghapus Promo "+nama_barang+" ?",
-            icon : "warning",
-            buttons: true,
-            dangerMode: true,
-          })
-          .then((willDelete) => {
-            if (willDelete) {
-             var app = this;
-             axios.delete(app.url+'/' + id)
-             .then(function (resp) {
-              app.getResults();
-              app.alert();            
-            })
-             .catch(function (resp) {
-               swal("Gagal Menghapus Promo", {
-                icon: "warning",
-              });
-             });
-           }
-           this.$router.replace('/setting-promo/');
-         });
+		this.$swal({
+			title: "Konfirmasi Hapus",
+			text : "Yakin Ingin Menghapus Promo "+nama_barang+" ?",
+			icon : "warning",
+			buttons: true,
+			dangerMode: true,
+		})
+		.then((willDelete) => {
+			if (willDelete) {
+				var app = this;
+				axios.delete(app.url+'/' + id)
+				.then(function (resp) {
+					app.getResults();
+					app.alert();            
+				})
+				.catch(function (resp) {
+					swal("Gagal Menghapus Promo", {
+						icon: "warning",
+					});
+				});
+			}
+			this.$router.replace('/setting-promo/');
+		});
 
-        }
+	}
 
 }
 }
