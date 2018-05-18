fa4951f93a5c02847841c7f40eb666c9c60813e5
Riko Pernando <rikopernando96@gmail.com>
2018-05-18 17:09:00 +0700

    buat folder khusus child component & configursainya
    buat folder khusus child component & configursainya

--- resources/assets/js/component.js
+++ resources/assets/js/component.js
@@ -1,27 +1,3 @@
-window.Vue = require('vue');
-import Vue from 'vue'
+import antrian from './components/child-component/antrian';
 
-Vue.component('antrian', {
-	props: ['list'],
-	template: '\
-	<tr>\
-	<td>\
-	<a href="#create-penjualan" v-on:click="changeAntrian">{{ list.no_antrian }}</a>\
-	</td>\
-	<td align="center">{{ list.pelanggan }}</td>\
-	<td align="right">{{ list.total_belanja }}</td>\
-	<td align="right">\
-	<a href="#create-penjualan" class="btn btn-xs btn-danger" v-on:click="deleteAntrian">Delete</a>\
-	</td>\
-	</tr>',
-	methods: {
-		deleteAntrian(){
-			this.$emit('deleteAntrian',this.list)
-		},
-		changeAntrian(){
-			this.$emit('changeAntrian',this.list)
-		}
-	}
-})
-
-export default Vue
+export default {antrian};
